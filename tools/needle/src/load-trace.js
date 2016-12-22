a7ce020b7a5a9c750b8e653190885c6311ee46bd
samcake <samuel.gateau@gmail.com>
2016-12-21 16:22:33 -0800

    Add the new files dah!

---
+++ tools/needle/src/load-trace.js
@@ -0,0 +1,100 @@
+/* Taken from :
+	filedrag.js - HTML5 File Drag & Drop demonstration
+	Featured on SitePoint.com
+	Developed by Craig Buckler (@craigbuckler) of OptimalWorks.net
+*/
+function TraceLoader(handler) {
+	this.self = this;
+	this.handler = handler;
+
+	// getElementById
+	function $id(id) {
+		return document.getElementById(id);
+	}
+
+
+	// output information
+	function Output(msg) {
+		var m = $id("messages");
+		m.innerHTML = msg + m.innerHTML;
+	}
+
+
+	// file drag hover
+	function FileDragHover(e) {
+		e.stopPropagation();
+		e.preventDefault();
+		e.target.className = (e.type == "dragover" ? "hover" : "");
+	}
+
+
+	// file selection
+	function FileSelectHandler(e) {
+
+		// cancel event and hover styling
+		FileDragHover(e);
+
+		// fetch FileList object
+		var files = e.target.files || e.dataTransfer.files;
+
+		// process all File objects
+		for (var i = 0, f; f = files[i]; i++) {
+			ParseFile(f);
+		}
+
+	}
+
+
+	// output file information
+	function ParseFile(file) {
+
+		Output(
+			"<p>File information: <strong>" + file.name +
+			"</strong> type: <strong>" + file.type +
+			"</strong> size: <strong>" + file.size +
+			"</strong> bytes</p>"
+		);
+
+			var reader = new FileReader();
+			reader.onload = function(e) {
+				var data = JSON.parse(e.target.result)
+			  var trace = new Trace(data);
+
+				self.handler(trace);
+
+			}
+			reader.readAsText(file);
+
+	}
+
+
+	// initialize
+	function Init(handler) {
+
+		var fileselect = $id("fileselect"),
+			filedrag = $id("filedrag")
+
+		// file select
+		fileselect.addEventListener("change", FileSelectHandler, false);
+
+		// is XHR2 available?
+		var xhr = new XMLHttpRequest();
+		if (xhr.upload) {
+
+			// file drop
+			filedrag.addEventListener("dragover", FileDragHover, false);
+			filedrag.addEventListener("dragleave", FileDragHover, false);
+			filedrag.addEventListener("drop", FileSelectHandler, false);
+			filedrag.style.display = "block";
+		}
+
+		self.handler = handler
+	}
+
+		// call initialization file
+		if (window.File && window.FileList && window.FileReader) {
+			Init(this.handler);
+		}
+
+
+}
