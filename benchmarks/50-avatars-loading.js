fd779d076e771134321472a499d02efe8672ffb0
samcake <samuel.gateau@gmail.com>
2017-02-13 17:29:37 -0800

    Updating the 50 avatar loading experience to focus on the texturing transfer etc

--- benchmarks/50-avatars-loading.js
+++ benchmarks/50-avatars-loading.js
@@ -13,8 +13,9 @@
 Script.include("./BenchmarkLib.js");
 
 var AVATAR_TRACING_RULES = "" + 
-    "trace.*=true\n" +
-    "*.detail=true\n" +
+    "trace.script*=false\n" +
+    "trace.render*=true\n" +
+    "trace.resource*=true\n" +
     "";
 
 Resources.overrideUrlPrefix(TEST_ROOT, Script.resolvePath(".."));
@@ -23,7 +24,7 @@ testScript.addTest({
     name: "50Avatars",
     loader: TestScript.locationLoader("hifi://dev-chris2.highfidelity.io/115.3,0.1,31.4/0,0.737,0, 0.676", 0),
     tracingRules: AVATAR_TRACING_RULES,
-    traceActions: TestScript.locationSteps([ {dt:1, pos:{x:99.9, y:0.0, z:22.2}, ori:{yaw:179}}
+    traceActions: TestScript.locationSteps([ {dt:1, pos:{x:99.9, y:2.0, z:22.2}, ori:{yaw:179}}
     									   ]),
     duration: 25
 });
