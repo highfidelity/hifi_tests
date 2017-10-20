f3b6a64c2867292fac69c4980df72c5b391a14cb
Nissim <nissim@highfidelity.io>
2017-10-20 10:24:23 -0700

    First version.

---
+++ tests/content/entity/zone/create/Position A.js
@@ -0,0 +1,5 @@
+// Position avatar
+MyAvatar.position  = {x: 7.0, y: 1.0, z: -19.0};
+MyAvatar.orientation = {x: 0.0, y: 1.0, z: 0.0, w: 0.0};
+
+exit();
---
+++ tests/content/entity/zone/create/Position C.js
@@ -0,0 +1,5 @@
+// Position avatar
+MyAvatar.position  = {x: 1.0, y: 1.0, z: -5.0};
+MyAvatar.orientation = {x: 0.0, y: 1.0, z: 0.0, w: 0.0};
+
+true;
