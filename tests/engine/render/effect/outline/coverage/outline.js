3086c16a148d847ee210a8035a72296402a24c6b
Olivier Prat <olivier@zvork.fr>
2017-10-27 12:06:16 +0200

    Modified outline test to be fully automatic with dynamic object creation in empty editable domain

--- tests/engine/render/effect/outline/coverage/outline.js
+++ tests/engine/render/effect/outline/coverage/outline.js
@@ -1,14 +1,86 @@
-var step = 0;
+var _step = 0;
+var createdEntities = [];
 var outlineConfig = Render.getConfig("RenderMainView.OutlineEffect0");
+
+print("Running Outline Test - press <SPACE> to go to next steps");
+Selection.clearSelectedItemsList("contextOverlayHighlightList");
+Selection.clearSelectedItemsList("contextOverlayHighlightList1");
+Selection.clearSelectedItemsList("contextOverlayHighlightList2");
+MyAvatar.goToLocation(
+    {x:0, y:0, z:0},
+    true,
+    Quat.angleAxis(0, {x: 0, y: 1, z: 0}),
+    true
+);
+
+resetConfig(Render.getConfig("RenderMainView.OutlineEffect0"));
+resetConfig(Render.getConfig("RenderMainView.OutlineEffect1"));
+resetConfig(Render.getConfig("RenderMainView.OutlineEffect2"));
+
+// all objects will have a finite lifetime
+var LIFETIME = 300; // 5 min
+
+// Initial setup
+var blueBox = Entities.addEntity({
+    type: "Box",
+    name: "BlueBox",
+    position: {x:-1, y:0.5, z:-1},
+    dimensions: {x:2, y:0.25, z:0.25},
+    dynamic: false,
+    lifetime: LIFETIME,
+    color: { red: 0, green: 0, blue: 155 }
+});
+var redSphere = Entities.addEntity({
+    type: "Sphere",
+    name: "RedSphere",
+    position: {x:1, y:1, z:-1},
+    dimensions: {x:0.5, y:0.5, z:0.5},
+    dynamic: false,
+    lifetime: LIFETIME,
+    color: { red: 200, green: 0, blue: 0 }
+});
+var greenSphere = Entities.addEntity({
+    type: "Sphere",
+    name: "GreenSphere",
+    position: {x:1, y:0.5, z:1},
+    dimensions: {x:1.75, y:2.5, z:0.5},
+    dynamic: false,
+    lifetime: LIFETIME,
+    color: { red: 0, green: 200, blue: 0 }
+});
+var snail = Entities.addEntity({
+    type: "Model",
+    name: "Snail",
+    position: {x:0, y:0, z:2},
+    dimensions: {x:2, y:2, z:2},
+    dynamic: false,
+    lifetime: LIFETIME,
+    modelURL: "https://hifi-content.s3.amazonaws.com/jimi/snail/snail_Dance2.fbx"
+});
+var terrain = Entities.addEntity({
+    type: "Model",
+    name: "Terrain",
+    position: {x:0, y:0, z:0},
+    dynamic: false,
+    lifetime: LIFETIME,
+    modelURL: "http://mpassets.highfidelity.com/e0f5d1a8-7062-40e3-ab03-3d4910b2f6c4-v1/Nevada-Moon-Rocks.fbx"
+});
+
+createdEntities.push(blueBox);
+createdEntities.push(redSphere);
+createdEntities.push(greenSphere);
+createdEntities.push(snail);
+createdEntities.push(terrain);
+
 var steps = [
     // Step 2
     function() {
-        Selection.addToSelectedItemsList("contextOverlayHighlightList", "entity", "{9b06a757-337c-459a-b288-a9c6fb29b723}")
+        Selection.addToSelectedItemsList("contextOverlayHighlightList", "entity", blueBox)
     },
     // Step 3
     function() {
-        Selection.addToSelectedItemsList("contextOverlayHighlightList", "entity", "{db5623cf-80b5-42bf-82c9-9cf265130a2a}")
-        Selection.addToSelectedItemsList("contextOverlayHighlightList", "entity", "{20a4b470-4846-4ec0-be22-97a2c12f598c}")
+        Selection.addToSelectedItemsList("contextOverlayHighlightList", "entity", redSphere)
+        Selection.addToSelectedItemsList("contextOverlayHighlightList", "entity", snail)
     },
     // Step 4
     function() {
@@ -51,14 +123,10 @@ var steps = [
     // Step 11
     function() {
         outlineConfig = Render.getConfig("RenderMainView.OutlineEffect2");
-        Selection.addToSelectedItemsList("contextOverlayHighlightList2", "entity", "{6328ca51-27e8-44e5-a328-51422f20392d}")
+        Selection.addToSelectedItemsList("contextOverlayHighlightList2", "entity", terrain)
+        Selection.addToSelectedItemsList("contextOverlayHighlightList2", "entity", greenSphere)
     },
     // Step 12
-    function() {
-        MyAvatar.goToLocation(MyAvatar.position, true, Quat.angleAxis(0, {x: 0, y: 1, z: 0}), true);
-        Selection.addToSelectedItemsList("contextOverlayHighlightList2", "entity", "{0489037e-13ae-4aae-8e71-8f629a088b88}")
-    },
-    // Step 13
     function() {
         outlineConfig["unoccludedFillOpacity"] = 1.0;        
         outlineConfig["occludedFillOpacity"] = 0.25;        
@@ -85,25 +153,21 @@ function resetConfig(config) {
     config["occludedFillOpacity"] = 0.0;
 }
 
-print("Running Outline Test - press <SPACE> to go to next steps");
-Selection.clearSelectedItemsList("contextOverlayHighlightList");
-Selection.clearSelectedItemsList("contextOverlayHighlightList1");
-Selection.clearSelectedItemsList("contextOverlayHighlightList2");
-MyAvatar.goToLocation(
-    {x:4.91982, y:0.271947, z:-54.7686},
-    true,
-    Quat.angleAxis(180, {x: 0, y: 1, z: 0}),
-    true
-);
-
-resetConfig(Render.getConfig("RenderMainView.OutlineEffect0"));
-resetConfig(Render.getConfig("RenderMainView.OutlineEffect1"));
-resetConfig(Render.getConfig("RenderMainView.OutlineEffect2"));
-
 Controller.keyPressEvent.connect(function(event){
-    print("Playing Outline Test - step "+(step+2));
-    steps[step]();
-    step++;
-    step = Math.min(step, steps.length-1);
+    if (event.key == 32) {
+        print("Playing Outline Test - _step "+(_step+2));
+        steps[_step]();
+        _step++;
+        _step = Math.min(_step, steps.length-1);
+    }
  });
  
+ // clean up after test
+Script.scriptEnding.connect(function () {
+    for (var i = 0; i < createdEntities.length; i++) {
+        Entities.deleteEntity(createdEntities[i]);
+    }
+    resetConfig(Render.getConfig("RenderMainView.OutlineEffect0"));
+    resetConfig(Render.getConfig("RenderMainView.OutlineEffect1"));
+    resetConfig(Render.getConfig("RenderMainView.OutlineEffect2"));
+});
--- tests/engine/render/effect/outline/coverage/test.md
+++ tests/engine/render/effect/outline/coverage/test.md
@@ -1,74 +1,44 @@
 # Engine Render Outline Effect
 
 ### Preconditions
-Interface is running, logged in the 'Dev-Welcome' domain at the initial position with avatar in third person view.
+Interface is running, logged in an empty region of a domain with editing rights and with avatar in third person view.
 
 ### Steps
 
-During all steps, rendering should be stable in time. The steps can be run manually or by executing the [outline.js script](./outline.js?raw=true) (from menu/Edit/Open and Run scripts From URL...) in which case each step is played in sequence by pressing the [SPACE] key.
+During all steps, rendering should be stable in time. Rune the [outline.js script](./outline.js?raw=true) (from menu/Edit/Open and Run scripts From URL...) and play each step in sequence by pressing the [SPACE] key.
 
 #### Step 1
-- Run the 'debugOutline.js' script located in the 'hifi/scripts/developer/utilities/render' folder.
 - Expected: ![](./init.jpg)
 
 #### Step 2
-- Enable the 'Hover select' checkbox, move the mouse cursor over the rug on the floor to the right and hover over it for 1 second. The rug should be surrounded by an orange outline.
-- Expected: ![](./firsthover.jpg)
+- Expected: ![](./step1.jpg)
 
 #### Step 3
-- Enable the 'Add selection' checkbox, move the mouse cursor over all three visible rugs and hover for 1 second in turn over them to select them.
-- Expected: ![](./threerugs.jpg)
+- Expected: ![](./step2.jpg)
 
 #### Step 4
-- Quickly move cursor to 'Hover select' checkbox and disable it to freeze the outlined objects (If another object is selected in the process you can uncheck 'Add selection' to reset selection and redo step 3). Change the 'Width' slider to approximately 4.
-- Expected: ![](./width4.jpg)
+- Expected: ![](./step3.jpg)
 
 #### Step 5
-- Enable the 'Glow' checkbox.
-- Expected: ![](./glow.jpg)
+- Expected: ![](./step4.jpg)
 
 #### Step 6
-- Set the 'Red', 'Green' and 'Blue' sliders in the 'Color' group box to respectively the values 0, 1 and 0.5
-- Expected: ![](./greenglow.jpg)
+- Expected: ![](./step5.jpg)
 
 #### Step 7
-- Set the 'Unoccluded' and 'Occluded' sliders in the 'Fill Opacity' group box to respectively the values 0.1, 0.5
-- Expected: ![](./fillwithglow.jpg)
+- Expected: ![](./step6.jpg)
 
 #### Step 8
-- Disable the 'Glow' checkbox.
-- Expected: ![](./fillwithoutglow.jpg)
+- Expected: ![](./step7.jpg)
 
 #### Step 9
-- Click on the 'Outl.1' tab to add selections to the second outline group.
-- Disable 'Add selection'
-- Enable 'Hover select'
-- Hover the mouse over your avatar.
-- Expected, the new outline should render on top of the previous one: ![](./outline2.jpg)
+- Expected: ![](./step8.jpg)
 
 #### Step 10
-- Quickly move mouse over the 'Hover select' checkbox so as not to loose the selection and disable 'Hover select'.
-- Enable 'Glow'
-- Set 'Width' to 5
-- Set 'Intensity' to 0.5
-- Set color 'Red', 'Green' and 'Blue' to respectively 1, 0 and 0
-- Set 'Unoccluded' fill opacity to 0.65
-- Expected: ![](./redoutline2.jpg)
+- Expected: ![](./step9.jpg)
 
 #### Step 11
-- Click on the 'Outl.2' tab to add selections to the third outline group.
-- Disable 'Add selection'
-- Enable 'Hover select'
-- Hover over the base of the tree at the top left of the screen.
-- Expected: the new outline should render over the first two![](./outline3.jpg)
+- Expected: the new outline should render over the first two![](./step10.jpg)
 
 #### Step 12
-- Turn view 180° and hover over the white wall at the back.
-- Expected: ![](./outline3-180.jpg)
-
-#### Step 13
-- Set 'Unoccluded' fill to 1 and 'Occluded' fill to 0.25
-- Hover over the white wall at the back.
-- Expected: ![](./outline3-fill.jpg)
-
- 
+- Expected: ![](./step11.jpg)
