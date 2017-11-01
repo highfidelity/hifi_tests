aefa08f53bae96db58ee420d2d727c5ecd58134c
Nissim Hadar <nissim.hadar@gmail.com>
2017-10-31 18:34:35 -0700

    WIP.

--- tests/content/entity/zone/ambientLightInheritance/setup.js
+++
@@ -1,4 +0,0 @@
-var avatarOriginOrientation = Quat.fromPitchYawRollDegrees(0.0, 0.0, 0.0);
-var avatarOriginPosition = MyAvatar.position;
-
-var objectProperties;
--- tests/content/entity/zone/ambientLightInheritance/test.js
+++ tests/content/entity/zone/ambientLightInheritance/test.js
@@ -1,8 +1,6 @@
-Script.include("setup.js?raw=true")
-
 // The models are loaded from the "MODEL_DIR_URL" located on github where we store all our test models
-
 var MODEL_DIR_URL = "https://github.com/highfidelity/hifi_tests/blob/master/assets/models/material_matrix_models/fbx/blender/";
+
 var MODEL_NAME_SUFFIX = ".fbx?raw=true";
 var MODEL_DIMS = {"x":0.809423565864563,"y":0.9995689988136292,"z":0.8092837929725647};
 var MODEL_SCALE = 0.75;
@@ -10,7 +8,9 @@ var UNIT = MODEL_SCALE * (MODEL_DIMS.x + MODEL_DIMS.z);
 var CELL_DIM = Vec3.multiply(UNIT, MODEL_DIMS);
 var BACKDROP_SIZE = 16;
 
-// Add the test Cases
+var avatarOriginOrientation = Quat.fromPitchYawRollDegrees(0.0, 0.0, 0.0);
+var avatarOriginPosition = MyAvatar.position;
+
 MyAvatar.orientation = avatarOriginOrientation;
 
 var objectOrientation = avatarOriginOrientation;
@@ -18,7 +18,7 @@ var objectPosition = {x: avatarOriginPosition.x, y: avatarOriginPosition.y + 0.7
 
 var objectName = "hifi_roughnessV00_metallicV_albedoV_ao";
 
-objectProperties = {
+var objectProperties = {
   type: "Model",
   modelURL: MODEL_DIR_URL + objectName + MODEL_NAME_SUFFIX,
   name: objectName,
@@ -55,9 +55,34 @@ var zone1properties = {
         color: {"red":255,"green":255,"blue":255},
         url: "http://hifi-content.s3.amazonaws.com/DomainContent/baked/island/Sky_Day-Sun-Mid-photo.ktx"
     }
-}
+};
 var zone1 = Entities.addEntity(zone1properties);
 
+Script.setTimeout(
+  function() {
+    MyAvatar.orientation = avatarOriginOrientation;
+    MyAvatar.position  = {x: avatarOriginPosition.x, y: avatarOriginPosition.y, z: avatarOriginPosition.z + 1.0};
+    
+    var newProperty = { 
+      position: {x: MyAvatar.position.x, y: MyAvatar.position.y + 1.0, z: MyAvatar.position.z - 3.0}
+    };
+    Entities.editEntity(object, newProperty);  
+  }, 
+    
+  2000);
+
+Script.setTimeout(
+  function() {
+    MyAvatar.orientation = avatarOriginOrientation;
+    MyAvatar.position  = {x: avatarOriginPosition.x, y: avatarOriginPosition.y, z: avatarOriginPosition.z + 1.0};
+    
+    var newProperty = { 
+      position: {x: MyAvatar.position.x, y: MyAvatar.position.y - 0.5, z: MyAvatar.position.z - 3.0}
+    };
+    Entities.editEntity(object, newProperty);  
+  }, 
+    
+  4000);
 // clean up after test
 Script.scriptEnding.connect(function () {
     Entities.deleteEntity(object);
