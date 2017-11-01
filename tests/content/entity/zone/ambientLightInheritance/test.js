a8d66b01be327dbeddc83c6de69b019e1155c9d5
Nissim Hadar <nissim.hadar@gmail.com>
2017-11-01 09:27:10 -0700

    WIP.

--- tests/content/entity/zone/ambientLightInheritance/test.js
+++ tests/content/entity/zone/ambientLightInheritance/test.js
@@ -6,15 +6,19 @@ var MODEL_DIMS = {"x":0.809423565864563,"y":0.9995689988136292,"z":0.80928379297
 var MODEL_SCALE = 0.75;
 var UNIT = MODEL_SCALE * (MODEL_DIMS.x + MODEL_DIMS.z);
 var CELL_DIM = Vec3.multiply(UNIT, MODEL_DIMS);
-var BACKDROP_SIZE = 16;
 
-var avatarOriginOrientation = Quat.fromPitchYawRollDegrees(0.0, 0.0, 0.0);
+// We are looking towards negative Z
+var OBJ_DX = 0.0;
+var OBJ_DY = 0.7;
+var OBJ_DZ = -2.5;
+
+var avatarLookDownZaxis = Quat.fromPitchYawRollDegrees(0.0, 0.0, 0.0);
 var avatarOriginPosition = MyAvatar.position;
 
-MyAvatar.orientation = avatarOriginOrientation;
+MyAvatar.orientation = avatarLookDownZaxis;
 
-var objectOrientation = avatarOriginOrientation;
-var objectPosition = {x: avatarOriginPosition.x, y: avatarOriginPosition.y + 0.7, z: avatarOriginPosition.z - 3.0};
+var objectOrientation = avatarLookDownZaxis;
+var objectPosition = {x: avatarOriginPosition.x + OBJ_DX, y: avatarOriginPosition.y + OBJ_DY, z: avatarOriginPosition.z  + OBJ_DZ};
 
 var objectName = "hifi_roughnessV00_metallicV_albedoV_ao";
 
@@ -30,13 +34,17 @@ var objectProperties = {
 };
 var object = Entities.addEntity(objectProperties);
 
+var zone1Dimensions = { x: 10.0, y: 10.0, z: 40.0};
+var zone2Dimensions = { x:  8.0, y: 10.0, z: 20.0};
+var zone3Dimensions = { x:  4.0, y: 10.0, z: 30.0};
+
 var zone1properties = {
     type: "Zone",
-    name: "Backdrop zone",
+    name: "Zone 1",
 
-    position: MyAvatar.position,    
+    position: {x: MyAvatar.position.x, y: MyAvatar.position.y - 2.0, z: MyAvatar.position.z - 25.0},
     rotation: MyAvatar.orientation,    
-    dimensions: {x: CELL_DIM.x * BACKDROP_SIZE, y:CELL_DIM.y * BACKDROP_SIZE , z: CELL_DIM.z* BACKDROP_SIZE},
+    dimensions: zone1Dimensions,
 
     keyLight:{
         color: {"red":255,"green":255,"blue":255},
@@ -48,8 +56,6 @@ var zone1properties = {
         intensity: 0.8
     },
 
-    hazeMode:"disabled",
-
     backgroundMode:"skybox",
     skybox:{
         color: {"red":255,"green":255,"blue":255},
@@ -58,35 +64,180 @@ var zone1properties = {
 };
 var zone1 = Entities.addEntity(zone1properties);
 
-Script.setTimeout(
-  function() {
-    MyAvatar.orientation = avatarOriginOrientation;
-    MyAvatar.position  = {x: avatarOriginPosition.x, y: avatarOriginPosition.y, z: avatarOriginPosition.z + 1.0};
-    
-    var newProperty = { 
-      position: {x: MyAvatar.position.x, y: MyAvatar.position.y + 1.0, z: MyAvatar.position.z - 3.0}
-    };
-    Entities.editEntity(object, newProperty);  
-  }, 
-    
-  2000);
+var zone2properties = {
+    type: "Zone",
+    name: "Zone 2",
 
+    position: {x: MyAvatar.position.x, y: MyAvatar.position.y - 2.0, z: MyAvatar.position.z - 25.0},
+    rotation: MyAvatar.orientation,    
+    dimensions: zone2Dimensions,
+
+    keyLight:{
+        color: {"red":255,"green":255,"blue":255},
+        direction: {
+            "x": 0.037007175385951996,
+            "y": -0.7071067690849304,
+            "z": -0.7061376571655273
+        },
+        intensity: 0.8
+    },
+
+    backgroundMode:"skybox",
+    skybox:{
+        color: {"red":255,"green":255,"blue":255},
+        url: "https://hifi-public.s3.amazonaws.com/images/SkyboxTextures/ThickCloudsWater2.jpg"
+    }
+};
+var zone2 = Entities.addEntity(zone2properties);
+
+var zone3properties = {
+    type: "Zone",
+    name: "Zone 3",
+
+    position: {x: MyAvatar.position.x, y: MyAvatar.position.y - 2.0, z: MyAvatar.position.z - 25.0},
+    rotation: MyAvatar.orientation,    
+    dimensions: zone3Dimensions,
+
+    keyLight:{
+        color: {"red":255,"green":255,"blue":255},
+        direction: {
+            "x": 0.037007175385951996,
+            "y": -0.7071067690849304,
+            "z": -0.7061376571655273
+        },
+        intensity: 0.8
+    },
+
+    backgroundMode:"skybox",
+    skybox:{
+        color: {"red":255,"green":255,"blue":255},
+        url: "https://hifi-public.s3.amazonaws.com/images/SkyboxTextures/FullMoon1024Compressed.jpg"
+    }
+};
+var zone3 = Entities.addEntity(zone3properties);
+
+// Show zone positions on the ground
+var marker1Dimensions = { x: 10.0, y: 0.01, z: 40.0};
+var marker2Dimensions = { x:  8.0, y: 0.01, z: 20.0};
+var marker3Dimensions = { x:  4.0, y: 0.01, z: 30.0};
+
+var marker1properties = {
+    type: "Box",
+    name: "marker 1",
+    position: {x: MyAvatar.position.x, y: MyAvatar.position.y - 5.0, z: MyAvatar.position.z - 25.0},
+    dimensions: marker1Dimensions,
+    "color": {"red":200,"green":0,"blue":0},
+    visible: true
+};
+var marker1 = Entities.addEntity(marker1properties);
+
+var marker2properties = {
+    type: "Box",
+    name: "marker 2",
+    position: {x: MyAvatar.position.x, y: MyAvatar.position.y - 5.0 + 0.01, z: MyAvatar.position.z - 25.0},
+    dimensions: marker2Dimensions,
+    "color": {"red":150,"green":150,"blue":0},
+    visible: true
+};
+var marker2 = Entities.addEntity(marker2properties);
+
+var marker3properties = {
+    type: "Box",
+    name: "marker 3",
+    position: {x: MyAvatar.position.x, y: MyAvatar.position.y - 5.0 + 0.02, z: MyAvatar.position.z - 25.0},
+    dimensions: marker3Dimensions,
+    "color": {"red":0,"green":200,"blue":0},
+    visible: true
+};
+var marker3 = Entities.addEntity(marker3properties);
+
+var STEP_TIME = 2000;
+var step = 1;
 Script.setTimeout(
   function() {
-    MyAvatar.orientation = avatarOriginOrientation;
-    MyAvatar.position  = {x: avatarOriginPosition.x, y: avatarOriginPosition.y, z: avatarOriginPosition.z + 1.0};
+    MyAvatar.orientation = avatarLookDownZaxis;
+    MyAvatar.position  = {x: avatarOriginPosition.x, y: avatarOriginPosition.y, z: avatarOriginPosition.z - 7.5};
     
     var newProperty = { 
-      position: {x: MyAvatar.position.x, y: MyAvatar.position.y - 0.5, z: MyAvatar.position.z - 3.0}
+      position: {x: MyAvatar.position.x + OBJ_DX, y: MyAvatar.position.y + OBJ_DY, z: MyAvatar.position.z + OBJ_DZ}
     };
     Entities.editEntity(object, newProperty);  
   }, 
     
-  4000);
+  step * STEP_TIME
+);
+
+step +=1;
+Script.setTimeout(
+  function() {
+    MyAvatar.orientation = avatarLookDownZaxis;
+    MyAvatar.position  = {x: avatarOriginPosition.x, y: avatarOriginPosition.y, z: avatarOriginPosition.z - 12.5};
+    
+    var newProperty = { 
+      position: {x: MyAvatar.position.x + OBJ_DX, y: MyAvatar.position.y + OBJ_DY, z: MyAvatar.position.z + OBJ_DZ}
+    };
+    Entities.editEntity(object, newProperty);  
+  }, 
+    
+  step * STEP_TIME
+);
+
+step +=1;
+Script.setTimeout(
+  function() {
+    MyAvatar.orientation = avatarLookDownZaxis;
+    MyAvatar.position  = {x: avatarOriginPosition.x + 3.0, y: avatarOriginPosition.y, z: avatarOriginPosition.z - 17.5};
+    
+    var newProperty = { 
+      position: {x: MyAvatar.position.x + OBJ_DX, y: MyAvatar.position.y + OBJ_DY, z: MyAvatar.position.z + OBJ_DZ}
+    };
+    Entities.editEntity(object, newProperty);  
+  }, 
+    
+  step * STEP_TIME
+);
+
+step +=1;
+Script.setTimeout(
+  function() {
+    MyAvatar.orientation = avatarLookDownZaxis;
+    MyAvatar.position  = {x: avatarOriginPosition.x, y: avatarOriginPosition.y, z: avatarOriginPosition.z - 17.5};
+    
+    var newProperty = { 
+      position: {x: MyAvatar.position.x + OBJ_DX, y: MyAvatar.position.y + OBJ_DY, z: MyAvatar.position.z + OBJ_DZ}
+    };
+    Entities.editEntity(object, newProperty);  
+  }, 
+    
+  step * STEP_TIME
+)
+
 // clean up after test
-Script.scriptEnding.connect(function () {
+step +=1;
+Script.setTimeout(
+  function () {
     Entities.deleteEntity(object);
     Entities.deleteEntity(zone1);
-});
+    Entities.deleteEntity(zone2);
+    Entities.deleteEntity(zone3);
+    Entities.deleteEntity(marker1);
+    Entities.deleteEntity(marker2);
+    Entities.deleteEntity(marker3);
+  },
+  
+  step * STEP_TIME
+);
 
+Script.scriptEnding.connect(
+    function () {
+        Entities.deleteEntity(object);
+        Entities.deleteEntity(zone1);
+        Entities.deleteEntity(zone2);
+
+        Entities.deleteEntity(zone3);
+        Entities.deleteEntity(marker1);
+        Entities.deleteEntity(marker2);
+        Entities.deleteEntity(marker3);
+    }
+);
 
