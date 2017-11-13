7344b472b9ba7d4a78b2446205583964ed2f3506
Sam Gateau <samuel.gateau@gmail.com>
2017-11-12 23:35:30 -0800

    rename folders and factorize zone creation

--- tests/content/entity/light/spot/create.js
+++ tests/content/entity/light/spot/create.js
@@ -32,6 +32,27 @@ var properties = {
 var light = Entities.addEntity(properties);
 createdEntities.push(light);
 
+var posOri2 = getStagePosOriAt(6.9, 4, 4)
+var lightOri2 = Quat.multiply(Quat.fromPitchYawRollDegrees(-45, 90, 0), posOri.ori);
+
+properties = {
+    lifetime: 120,  
+    type: "light",  
+    name: "test create spot light",
+    position: posOri2.pos,
+    rotation: lightOri2,
+  
+    type: "Light",
+    isSpotlight: true,
+    color: { red: 255, green: 125, blue: 255 },
+    intensity: 2.0,
+    falloffRadius: 6.0,
+    exponent: 1,
+    cutoff: 20,
+    dimensions: { x: 8.0, y: 8.0, z: 12.0 }, 
+  };
+  createdEntities.push(Entities.addEntity(properties));
+
 properties = Entities.getEntityProperties(light);
 print("Light added :" + light);
 print(JSON.stringify(properties));
