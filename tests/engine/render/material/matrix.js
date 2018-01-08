c95875c2f70905a362cfb47ce359fb51322eb134
Olivier Prat <olivier@zvork.fr>
2018-01-08 14:55:41 +0100

    Updated material test with latest update from PR #12028

--- tests/engine/render/material/matrix.js
+++ tests/engine/render/material/matrix.js
@@ -21,112 +21,11 @@ var MODEL_SCALE = 0.75;
 var MODEL_SPIN = 0.0;
 var ROOT_Y_OFFSET = -0.1;
 var ROOT_Z_OFFSET = 3.0;
-<<<<<<< HEAD
-var LIFETIME = 360;
-var BACKDROP_SIZE = 16;
-=======
 var LIFETIME = 120;
->>>>>>> a69673cf1a63cbdf69b0012022b7977868441bf2
 
 function addTestBackdropLocal(name, position, orientation) {
     
-<<<<<<< HEAD
-    var cellDim = Vec3.multiply(unit, MODEL_DIMS);
-    
-    var up = Quat.getUp(orientation)
-    var right = Quat.getRight(orientation)
-    var forward = Quat.getForward(orientation)
-    var far = Vec3.sum(position, Vec3.multiply(5 * unit, forward))
-    var under = Vec3.sum(position, Vec3.multiply(-1.5 * unit, up))
-    var under_left_near = Vec3.sum(Vec3.multiply(-5 * unit, forward), Vec3.sum(under, Vec3.multiply(-2.0 * unit, right)))
-
-    var lightDir = Vec3.normalize(Vec3.sum(Vec3.multiply(-1, up),
-                                           Vec3.multiply(-1, right)))
-
-    backdrop.push(Entities.addEntity({
-        type: "Shape",
-        shape: "Cube",
-        name: "Backdrop",
-        position: under,    
-        rotation: orientation,    
-        dimensions: {x: cellDim.x * BACKDROP_SIZE, y:cellDim.y , z: BACKDROP_SIZE *cellDim.z},
-        lifetime: LIFETIME,
-    }));
-
- 
-    backdrop.push(Entities.addEntity({
-        type: "Shape",
-        shape: "Cube",
-        name: "Backdrop",
-        position: far,    
-        rotation: orientation,    
-        dimensions: {x: cellDim.x * BACKDROP_SIZE, y:cellDim.y * BACKDROP_SIZE , z: cellDim.z},
-        lifetime: LIFETIME,
-    }));
-
-    backdrop.push(Entities.addEntity({
-        type: "Zone",
-        name: "Backdrop zone",
-  
-        position: under,    
-        rotation: orientation,    
-        dimensions: {x: cellDim.x * BACKDROP_SIZE, y:cellDim.y * BACKDROP_SIZE , z: cellDim.z* BACKDROP_SIZE},
-        lifetime: LIFETIME,
-  
-        keyLight:{
-            ambientIntensity: 1,
-            color: {"red":255,"green":255,"blue":255},
-            direction: {
-                "x": 0.037007175385951996,
-                "y": -0.7071067690849304,
-                "z": -0.7061376571655273
-            },
-            intensity: 0.8
-        },
-
-        hazeMode:"disabled",
-
-        backgroundMode:"skybox",
-        skybox:{
-            color: {"red":255,"green":255,"blue":255},
-            url: "http://hifi-content.s3.amazonaws.com/DomainContent/baked/island/Sky_Day-Sun-Mid-photo.ktx"
-        }
-    }));
-=======
     var backdrop = setupStage(true, true)
->>>>>>> a69673cf1a63cbdf69b0012022b7977868441bf2
-
-    backdrop.push(Entities.addEntity({
-        type: "Light",
-        name: "Point Light",
-  
-        position: under,    
-        rotation: orientation,    
-        dimensions: {x: cellDim.x * BACKDROP_SIZE, y:cellDim.y * BACKDROP_SIZE , z: cellDim.z* BACKDROP_SIZE},
-        lifetime: LIFETIME,
-  
-        color: {"red":100,"green":120,"blue":255},
-        intensity: 2.0,
-        falloffRadius: 5.0,
-        isSpotlight: false
-    }));
-
-    backdrop.push(Entities.addEntity({
-        type: "Light",
-        name: "Spot Light",
-  
-        position: under_left_near,    
-        rotation: orientation,    
-        dimensions: {x: cellDim.x * BACKDROP_SIZE, y:cellDim.y * BACKDROP_SIZE , z: cellDim.z* BACKDROP_SIZE},
-        lifetime: LIFETIME,
-  
-        color: {"red":255,"green":200,"blue":200},
-        intensity: 5.0,
-        falloffRadius: 15.0,
-        isSpotlight: true,
-        cutoff: 60.0,
-        exponent: 10.0
-    }));
 
     return backdrop;
 }
