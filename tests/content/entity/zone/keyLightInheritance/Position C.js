35ea81522acacb7de8d2ffe55f969d526566f657
Nissim Hadar <nissim.hadar@gmail.com>
2018-01-12 11:18:12 -0800

    Removed tests that are not ready.

--- tests/content/entity/zone/keyLightInheritance/Position C.js
+++
@@ -1,5 +0,0 @@
-// Position avatar
-MyAvatar.position  = {x: 1.0, y: 1.0, z: -5.0};
-MyAvatar.orientation = {x: 0.0, y: 1.0, z: 0.0, w: 0.0};
-
-true;
--- tests/content/entity/zone/keyLightInheritance/Position D.js
+++
@@ -1,5 +0,0 @@
-// Position avatar
-MyAvatar.position  = {x: 0.0, y: 1.0, z: 8.0};
-MyAvatar.orientation = {x: 0.0, y: 0.0, z: 0.0, w: 0.0};
-
-true;
