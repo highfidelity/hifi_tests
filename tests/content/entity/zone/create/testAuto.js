1fb20be14a50b2d9ff7dd2d47ef344c5f5012cc5
NissimHadar <nissim.hadar@gmail.com>
2018-02-23 18:40:02 -0800

    WIP - recursive tests.

--- tests/content/entity/zone/create/testAuto.js
+++ tests/content/entity/zone/create/testAuto.js
@@ -1,4 +1,6 @@
-var autoTester = Script.require("../../../../utils/autoTester.js" );
+var autoTester = Script.require("https://raw.githubusercontent.com/highfidelity/hifi_tests/master/tests/utils/autoTester.js" );
+//var autoTester = Script.require("../../../../utils/autoTester.js" );
+
 autoTester.enableAuto();
 
 Script.include("./test.js?raw=true");
--- tests/content/entity/zone/testRecursive.js
+++ tests/content/entity/zone/testRecursive.js
@@ -1,7 +1,9 @@
-var autoTester = Script.require("../../../utils/autoTester.js" );
+// This is an automatically generated file, created by auto-tester
+
+var autoTester = Script.require("https://github.com/highfidelity/hifi_tests/blob/master/tests/utils/autoTester.js?raw=true");
 autoTester.enableRecursive();
 
-Script.include("ambientLightInheritance/test.js?raw=true");
-Script.include("create/test.js?raw=true");
+Script.include("file:///D:/GitHub/hifi-tests/tests/content/entity/zone/ambientLightInheritance/test.js?raw=true");
+Script.include("file:///D:/GitHub/hifi-tests/tests/content/entity/zone/create/test.js?raw=true");
 
 autoTester.runRecursive();
