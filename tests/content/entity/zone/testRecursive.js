df3548643422c282567e06d62d6e7064eb32e36b
NissimHadar <nissim.hadar@gmail.com>
2018-03-07 10:47:56 -0800

    WIP - adding the creation of recursive scripts.

--- tests/content/entity/zone/testRecursive.js
+++ tests/content/entity/zone/testRecursive.js
@@ -1,16 +1,9 @@
 // This is an automatically generated file, created by auto-tester
-// #debug
 
-////var autoTester = Script.require("https://github.com/highfidelity/hifi_tests/blob/master/tests/utils/autoTester.js?raw=true");
-////var autoTester = Script.require("../../../utils/autoTester.js" );
-var autoTester = Script.require("https://raw.githubusercontent.com/NissimHadar/hifi_tests/addRecursionToAutotester/tests/utils/autoTester.js" );
-
+var autoTester = Script.require("https://raw.githubusercontent.com/NissimHadar/hifi_tests/addRecursionToAutotester/tests/utils/autoTester.js");
 autoTester.enableRecursive();
 
-////Script.include("file:///D:/GitHub/hifi-tests/tests/content/entity/zone/ambientLightInheritance/test.js?raw=true");
-////Script.include("file:///D:/GitHub/hifi-tests/tests/content/entity/zone/create/test.js");
-////Script.include("file:///D:/GitHub/hifi-tests/tests/content/entity/zone/ambientLightInheritance/test.js?raw=true");
-Script.include("https://raw.githubusercontent.com/NissimHadar/hifi_tests/addRecursionToAutotester/tests/content/entity/zone/create/test.js");
 Script.include("https://raw.githubusercontent.com/NissimHadar/hifi_tests/addRecursionToAutotester/tests/content/entity/zone/ambientLightInheritance/test.js");
+Script.include("https://raw.githubusercontent.com/NissimHadar/hifi_tests/addRecursionToAutotester/tests/content/entity/zone/create/test.js");
 
 autoTester.runRecursive();
--- tests/testRecursive.js
+++ tests/testRecursive.js
@@ -1,10 +1,20 @@
 // This is an automatically generated file, created by auto-tester
 
-var autoTester = Script.require("https://github.com/highfidelity/hifi_tests/blob/master/tests/utils/autoTester.js?raw=true");
+var autoTester = Script.require("https://raw.githubusercontent.com/NissimHadar/hifi_tests/addRecursionToAutotester/tests/utils/autoTester.js");
 autoTester.enableRecursive();
 
-Script.include("file:///D:/GitHub/hifi-tests/tests/content/entity/zone/ambientLightInheritance/test.js?raw=true");
-Script.include("file:///D:/GitHub/hifi-tests/tests/content/entity/zone/create/test.js?raw=true");
-Script.include("file:///D:/GitHub/hifi-tests/tests/engine/render/effect/haze/test.js?raw=true");
+Script.include("https://raw.githubusercontent.com/NissimHadar/hifi_tests/addRecursionToAutotester/tests/content/entity/light/point/create/test.js");
+Script.include("https://raw.githubusercontent.com/NissimHadar/hifi_tests/addRecursionToAutotester/tests/content/entity/light/spot/create/test.js");
+Script.include("https://raw.githubusercontent.com/NissimHadar/hifi_tests/addRecursionToAutotester/tests/content/entity/material/apply/avatars/test.js");
+Script.include("https://raw.githubusercontent.com/NissimHadar/hifi_tests/addRecursionToAutotester/tests/content/entity/material/apply/entities/model/test.js");
+Script.include("https://raw.githubusercontent.com/NissimHadar/hifi_tests/addRecursionToAutotester/tests/content/entity/material/apply/entities/shape/test.js");
+Script.include("https://raw.githubusercontent.com/NissimHadar/hifi_tests/addRecursionToAutotester/tests/content/entity/material/apply/overlays/model/test.js");
+Script.include("https://raw.githubusercontent.com/NissimHadar/hifi_tests/addRecursionToAutotester/tests/content/entity/material/create/test.js");
+Script.include("https://raw.githubusercontent.com/NissimHadar/hifi_tests/addRecursionToAutotester/tests/content/entity/model/physics/test.js");
+Script.include("https://raw.githubusercontent.com/NissimHadar/hifi_tests/addRecursionToAutotester/tests/content/entity/shape/create/test.js");
+Script.include("https://raw.githubusercontent.com/NissimHadar/hifi_tests/addRecursionToAutotester/tests/content/entity/shape/physics/test.js");
+Script.include("https://raw.githubusercontent.com/NissimHadar/hifi_tests/addRecursionToAutotester/tests/content/entity/zone/ambientLightInheritance/test.js");
+Script.include("https://raw.githubusercontent.com/NissimHadar/hifi_tests/addRecursionToAutotester/tests/content/entity/zone/create/test.js");
+Script.include("https://raw.githubusercontent.com/NissimHadar/hifi_tests/addRecursionToAutotester/tests/engine/render/effect/haze/test.js");
 
 autoTester.runRecursive();
