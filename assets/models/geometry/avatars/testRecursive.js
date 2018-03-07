82ac924da974305aaca80408e9be83cc82cd431a
NissimHadar <nissim.hadar@gmail.com>
2018-03-07 13:25:04 -0800

    Added recursive scripts using auto-tester.

---
+++ assets/models/geometry/avatars/testRecursive.js
@@ -0,0 +1,7 @@
+// This is an automatically generated file, created by auto-tester
+
+var autoTester = Script.require("https://raw.githubusercontent.com/NissimHadar/hifi_tests/addRecursionToAutotester/tests/utils/autoTester.js");
+autoTester.enableRecursive();
+
+
+autoTester.runRecursive();
---
+++ tests/content/entity/shape/testRecursive.js
@@ -0,0 +1,9 @@
+// This is an automatically generated file, created by auto-tester
+
+var autoTester = Script.require("https://raw.githubusercontent.com/NissimHadar/hifi_tests/addRecursionToAutotester/tests/utils/autoTester.js");
+autoTester.enableRecursive();
+
+Script.include("https://raw.githubusercontent.com/NissimHadar/hifi_tests/addRecursionToAutotester/tests/content/entity/shape/create/test.js");
+Script.include("https://raw.githubusercontent.com/NissimHadar/hifi_tests/addRecursionToAutotester/tests/content/entity/shape/physics/test.js");
+
+autoTester.runRecursive();
