383b664abf243f60c5dcaa6e2b3a8a83a1ece431
NissimHadar <nissim.hadar@gmail.com>
2018-03-08 19:04:40 -0800

    WIP - haze test.

--- tests/engine/render/effect/haze/testAuto.js
+++ tests/engine/render/effect/haze/testAuto.js
@@ -1,5 +1,4 @@
-//var autoTester = Script.require("https://raw.githubusercontent.com/highfidelity/hifi_tests/master/tests/utils/autoTester.js" );
-var autoTester = Script.require("../../../../utils/autoTester.js" );
+var autoTester = Script.require("https://raw.githubusercontent.com/NissimHadar/hifi_tests/addRecursionToAutotester/tests/utils/autoTester.js" );
 
 autoTester.enableAuto();
 
