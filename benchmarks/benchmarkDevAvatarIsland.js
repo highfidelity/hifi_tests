9f75ace30e2970334ea971b08a8e3ba10f5afd0e
NissimHadar <nissim.hadar@gmail.com>
2018-03-15 16:10:46 -0700

    Added possibility to delay start of test

--- benchmarks/benchmarkDevAvatarIsland.js
+++ benchmarks/benchmarkDevAvatarIsland.js
@@ -19,17 +19,17 @@ Resources.overrideUrlPrefix(TEST_ROOT, Script.resolvePath(".."));
 var testScript = new TestScript();
 testScript.addTest({
     name: "benchmarkDevAvatarIsland",
-    loader: TestScript.locationLoader("hifi://dev-AvatarIsland.highfidelity.io/14.9,-9.6,25.7/0,1.0,0.0,0.0", 1),
+    loader: TestScript.locationLoader("hifi://dev-AvatarIsland.highfidelity.io/14.9,-9.6,25.7/0,1.0,0.0,0.0", 1, null, null, 180),
     tracingRules: AVATAR_TRACING_RULES,
     traceActions: TestScript.locationSteps([
-        {dt:2, pos:{x:14.9, y:-9.6, z: 25.7}, ori:{yaw:  0   }},
-        {dt:2, pos:{x:12.5, y:-8.1, z: -8.9}, ori:{yaw: -58.3}},
-        {dt:2, pos:{x:17.8, y:-8.1, z:-10.1}, ori:{yaw: -89.6}},
-        {dt:2, pos:{x:22.5, y:-4.7, z: -5.4}, ori:{yaw:  60.9}},
-        {dt:2, pos:{x: 9.8, y:-8.3, z: -4.9}, ori:{yaw:112.6 }},
-        {dt:2, pos:{x:63.5, y:-2.1, z: 11.2}, ori:{yaw: 72.9 }}        
+        {dt:6, pos:{x:14.9, y:-9.6, z: 25.7}, ori:{yaw:  0   }},
+        {dt:6, pos:{x:12.5, y:-8.1, z: -8.9}, ori:{yaw: -58.3}},
+        {dt:6, pos:{x:17.8, y:-8.1, z:-10.1}, ori:{yaw: -89.6}},
+        {dt:6, pos:{x:22.5, y:-4.7, z: -5.4}, ori:{yaw:  60.9}},
+        {dt:6, pos:{x: 9.8, y:-8.3, z: -4.9}, ori:{yaw:112.6 }},
+        {dt:6, pos:{x:63.5, y:-2.1, z: 11.2}, ori:{yaw: 72.9 }}        
    ]),
-    duration: 12
+    duration: 36
 });
 
 
