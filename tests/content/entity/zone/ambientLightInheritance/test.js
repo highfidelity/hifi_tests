f1196f92dadb6ed19e41d3e9c0837e9d35a29258
NissimHadar <nissim.hadar@gmail.com>
2018-04-05 09:55:43 -0700

    Made test advance key a variable.

--- tests/content/entity/zone/ambientLightInheritance/test.js
+++ tests/content/entity/zone/ambientLightInheritance/test.js
@@ -1,6 +1,6 @@
-var user = "NissimHadar/";
+var user = "highfidelity/";
 var repository = "hifi_tests/";
-var branch = "needleUpdate/";
+var branch = "master/";
 var autoTester = Script.require("https://github.com/" + user + repository + "blob/" + branch + "tests/utils/autoTester.js?raw=true" );
 
 autoTester.perform("Zone - Ambient Light Inheritance", Script.resolvePath("."), function(testType) {
