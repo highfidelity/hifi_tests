04cdf458fc35f5ed3797e95e6cf69724f0b69d37
Olivier Prat <olivier@zvork.fr>
2018-04-16 11:13:56 +0200

    Added setup script for haze tests

---
+++ tests/engine/render/effect/haze/none/test.js
@@ -0,0 +1,22 @@
+var user = "highfidelity/";
+var repository = "hifi_tests/";
+var branch = "master/";
+var autoTester = Script.require("https://github.com/" + user + repository + "blob/" + branch + "tests/utils/autoTester.js?raw=true" );
+
+autoTester.perform("Haze - off", Script.resolvePath("."), function(testType) {
+    var spectatorCameraConfig = autoTester.setupTest();
+
+    // Test material matrix
+    Script.include("../setup.js?raw=true")
+
+    // Setup
+    var createdEntities = setup(null,spectatorCameraConfig)
+
+    autoTester.addStepSnapshot("Clean up after test", function () {
+        for (var i = 0; i < createdEntities.length; i++) {
+            Entities.deleteEntity(createdEntities[i]);
+        }
+    });
+    
+    var result = autoTester.runTest(testType);
+});
---
+++ tests/engine/render/effect/haze/setup.js
@@ -0,0 +1,120 @@
+// Test haze setup
+
+// To use this script, include it, and define an object with all the haze properties, get the spectatorCameraConfig from the
+// autoTester and call "setup(hazeProperties, spectatorCameraConfig)",
+// the script will create the correct zone and objects and move the Avatar and return the list of entities in an array.
+// If null is sent, then haze will be turned off.
+// Example:
+// var HAZE = {
+//    hazeRange: 500.0,
+//    hazeColor:{"red":153,"green":107,"blue":47}
+//    hazeAltitudeEffect: 1,  
+// };
+// var entities = setup(HAZE, spectatorCameraConfig);
+// 
+// The models are loaded from the "MODEL_DIR_URL" located on github where we store all our test models
+
+var ASSETS_URL = "https://github.com/highfidelity/hifi_tests/blob/master/assets/"
+var MODEL_DIR_URL = ASSETS_URL + "models/material_matrix_models/fbx/blender/";
+var SUFFIX = "?raw=true";
+var MODEL_NAME_SUFFIX = ".fbx"+SUFFIX;
+var TOWER_MODEL_SCALE = 20.0;
+var MODEL_SCALE = 1.0;
+var LIFETIME = 120;
+var MODEL_COUNT = 10;
+
+setup = function (hazeDef, spectatorCameraConfig) {
+    var pos =  { x: 1000, y: -320, z: 1000 };
+    var entities = []
+    // Initial setup
+    MyAvatar.goToLocation(
+        pos,
+        true,
+        Quat.angleAxis(180, { x: 0, y: 1, z: 0 }),
+        true
+    );
+    
+    entities.push( Entities.addEntity({
+        type: 'Box',
+        name: 'Terrain',
+        shape: 'Cube',
+        dimensions: { x: 2000.0, y: 0.2, z: 2000.0 },
+        position: { x: pos.x, y: pos.y - 3.0, z: pos.z },
+        color: { "blue": 200, "green": 200, "red": 200 }
+    }) );
+
+    var i;
+    var url = MODEL_DIR_URL + "hifi_albedoM_ao" + MODEL_NAME_SUFFIX;
+    for (i=0 ; i<MODEL_COUNT ; i++) {
+        entities.push( Entities.addEntity({
+            type: "Model",
+            modelURL: url,
+            name: "hifi_albedoM_ao",
+            position: { x: pos.x, y: pos.y + 1.0 + i*TOWER_MODEL_SCALE, z: pos.z - 500.0},    
+            dimensions: {x:TOWER_MODEL_SCALE, y:TOWER_MODEL_SCALE, z:TOWER_MODEL_SCALE},
+            lifetime: LIFETIME,
+        }) );
+    }
+    entities.push( Entities.addEntity({
+        type: "Model",
+        modelURL: url,
+        name: "hifi_albedoM_ao",
+        position: { x: pos.x+1.0, y: pos.y, z: pos.z - 3.0},    
+        dimensions: {x:MODEL_SCALE, y:MODEL_SCALE, z:MODEL_SCALE},
+        lifetime: LIFETIME,
+    }) );
+    entities.push( Entities.addEntity({
+        type: "Model",
+        modelURL: url,
+        name: "hifi_albedoM_ao",
+        position: { x: pos.x-1.0, y: pos.y, z: pos.z - 3.0},    
+        dimensions: {x:MODEL_SCALE, y:MODEL_SCALE, z:MODEL_SCALE},
+        lifetime: LIFETIME,
+    }) );
+
+    var hazeMode;
+
+    if (hazeDef) {
+        hazeMode = "enabled";
+    }
+
+    var SKY_URL = Script.resolvePath(ASSETS_URL + 'skymaps/Sky_Day-Sun-Mid-photo.ktx' + SUFFIX);
+    var sky = Entities.addEntity({
+        type: "Zone",
+        name: "Sky",
+
+        position: {x: pos.x, y: pos.y - 2.0, z: pos.z - 25.0},
+        rotation: MyAvatar.orientation,    
+        dimensions: { x: 10000.0, y: 600.0, z: 10000.0 },
+
+        keyLightMode: "enabled",
+        keyLight:{
+            color: {"red":255, "green":255, "blue":255},
+            direction: {
+                x:  0.16317591071128845,
+                y: -0.3420201241970062,
+                z:  0.9254165291786194
+            },
+            intensity: 0.8
+        },
+
+        skyboxMode: "enabled",
+        skybox: {
+            color: {"red":255,"green":255,"blue":255},
+            url: SKY_URL
+        },
+        
+        ambientLightMode: "enabled",
+        ambientLight: {
+            ambientURL: SKY_URL
+        },
+
+        hazeMode: hazeMode,
+        haze: hazeDef
+    });
+    entities.push(sky)
+
+    spectatorCameraConfig.position = {x: pos.x, y: pos.y + 0.6, z: pos.z};
+
+    return entities;
+}
