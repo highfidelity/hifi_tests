01eb77ebe0995111df1383b0091caf50244a791f
Olivier Prat <olivier@zvork.fr>
2018-04-16 18:23:27 +0200

    Started splitting shadow tests in multiple folders

---
+++ tests/engine/render/shadows/normal/test.js
@@ -0,0 +1,23 @@
+var user = "highfidelity/";
+var repository = "hifi_tests/";
+var branch = "master/";
+var autoTester = Script.require("https://github.com/" + user + repository + "blob/" + branch + "tests/utils/autoTester.js?raw=true" );
+
+autoTester.perform("Point light", Script.resolvePath("."), function(testType) {
+    var spectatorCameraConfig = autoTester.setupTest();
+
+    // Test material matrix
+    Script.include("../setup.js?raw=true")
+
+    spectatorCameraConfig.position = { x: TEST_POSITION.x, y: TEST_POSITION.y, z: TEST_POSITION.z - 0.6 };
+    // Add the test Cases
+    var createdEntities = setup(60.0, 60.0);
+
+    autoTester.addStepSnapshot("Clean up after test", function () {
+        for (var i = 0; i < createdEntities.length; i++) {
+            Entities.deleteEntity(createdEntities[i]);
+        }
+    });
+    
+    var result = autoTester.runTest(testType);
+});
