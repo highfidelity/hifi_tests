ce2661517b8332be4ff4f5b6fe5b5a68b0e699cc
NissimHadar <nissim@highfidelity.io>
2018-05-02 11:59:26 -0700

    Removed setting snapshot location and URL from batch tests.

--- tests/engine/render/shadows/front/testBatch.js
+++ tests/engine/render/shadows/front/testBatch.js
@@ -3,12 +3,9 @@ var repository = "hifi_tests/";
 var branch = "master/";
 var autoTester = Script.require("https://github.com/" + user + repository + "blob/" + branch + "tests/utils/autoTester.js?raw=true" );
 
-Snapshot.setSnapshotsLocation("D:/t/");
-
 autoTester.enableAuto();
 autoTester.enableBatch();
 
-Window.location = "localhost/8000,8000,8000/0.0,0.0,0.0,1.0";
 Test.wait(3000);
 
 Script.include("./test.js?raw=true");
