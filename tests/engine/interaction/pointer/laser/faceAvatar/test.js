405040c8b1220056f9852f6a7f20c3a3257fbda4
NissimHadar <nissim.hadar@gmail.com>
2018-05-18 16:29:47 -0700

    Don't always define GitHub repo, user and branch.

--- tests/engine/interaction/pointer/laser/faceAvatar/test.js
+++ tests/engine/interaction/pointer/laser/faceAvatar/test.js
@@ -1,6 +1,7 @@
-var user = "highfidelity/";
-var repository = "hifi_tests/";
-var branch = "master/";
+if (typeof user === 'undefined') var user = "highfidelity/";var user = "highfidelity/";
+if (typeof repository === 'undefined') var repository = "hifi_tests/";
+if (typeof branch === 'undefined') var branch = "master/";
+
 var autoTester = Script.require("https://github.com/" + user + repository + "blob/" + branch + "tests/utils/autoTester.js?raw=true" );
 
 autoTester.perform("LaserPointer faceAvatar test", Script.resolvePath("."), function(testType) {
