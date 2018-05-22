78d0b8343cd9474f00e7d5dc348256a39a3b3e73
NissimHadar <nissim.hadar@gmail.com>
2018-05-22 08:55:02 -0700

    Debugging.

--- tests/utils/autoTester.js
+++ tests/utils/autoTester.js
@@ -175,6 +175,10 @@ module.exports.setupTest = function (primaryCamera) {
         return;
     }
     
+    // Make sure camera is in correct mode
+    var prevCameraMode = Camera.mode;
+    Camera.mode = "first person";
+    
     // Use a specific avatar.  This is needed because we want the avatar's height to be fixed.
     previousSkeletonURL = MyAvatar.skeletonModelURL;
     MyAvatar.skeletonModelURL = "https://highfidelity.com/api/v1/commerce/entity_edition/813addb9-b985-49c8-9912-36fdbb57e04a.fst?certificate_id=MEUCIQDgYR2%2BOrCh5HXeHCm%2BkR0a2JniEO%2BY4y9tbApxCAPo4wIgXZEQdI4cQc%2FstAcr9tFT9k4k%2Fbuj3ufB1aB4W0tjIJc%3D";
@@ -183,10 +187,6 @@ module.exports.setupTest = function (primaryCamera) {
     if (typeof Test !== 'undefined') {
         Test.waitIdle();
     }
-    
-    // Make sure camera is in correct mode
-    var prevCameraMode = Camera.mode;
-    Camera.mode = "first person";
 
     // Set Avatar to T-pose
     for (var i = 0; i < MyAvatar.getJointNames().length; ++i) {
