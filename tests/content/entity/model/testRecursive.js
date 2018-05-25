6ca80f94a86b9b16290d055b07e9b99cc171f7f5
NissimHadar <nissim.hadar@gmail.com>
2018-05-24 17:57:11 -0700

    Added missing semicolons

--- tests/content/entity/model/testRecursive.js
+++ tests/content/entity/model/testRecursive.js
@@ -1,8 +1,8 @@
-// This is an automatically generated file, created by auto-tester on May 24 2018, 13:34
+// This is an automatically generated file, created by auto-tester on May 24 2018, 17:56
 
-user = "highfidelity/"
-repository = "hifi_tests/"
-branch = "master/"
+user = "highfidelity/;"
+repository = "hifi_tests/;"
+branch = "master/;"
 
 var autoTester = Script.require("https://github.com/highfidelity/hifi_tests/blob/master/tests/utils/autoTester.js?raw=true");
 
