b50f5683a27f0d072bfe6f4e47173567475bb4fa
NissimHadar <nissim@highfidelity.io>
2018-06-06 15:21:51 -0700

    Updated version.

--- tests/content/entity/material/apply/avatars/test.js
+++ tests/content/entity/material/apply/avatars/test.js
@@ -4,10 +4,7 @@ if (typeof branch === 'undefined') branch = "master/";
 
 var autoTester = Script.require("https://github.com/" + user + repository + "blob/" + branch + "tests/utils/autoTester.js?raw=true" );
 
-autoTester.perform("Apply Material Entities to Avatars", Script.resolvePath("."), function(testType) {
-    var spectatorCameraConfig = autoTester.setupTest(true);
-    spectatorCameraConfig.position = { x: MyAvatar.position.x, y: MyAvatar.position.y, z: MyAvatar.position.z - 0.6 };
-
+autoTester.perform("Apply Material Entities to Avatars", Script.resolvePath("."), "primary", function(testType) {
     Script.include("../../../../../utils/test_stage.js?raw=true");
 
     // Add the test Cases
@@ -20,24 +17,46 @@ autoTester.perform("Apply Material Entities to Avatars", Script.resolvePath(".")
 
     MyAvatar.orientation = Quat.fromVec3Degrees({x:0, y:180, z:0});
 
-    var prevCameraMode = Camera.mode;
-    Camera.mode = "mirror";
-
     var LIFETIME = 120;
 
     createdEntities.push(Entities.addEntity({
-                  type: "Material",
-                  materialURL: "materialData",
-                  position: MyAvatar.position,
-                  materialData: JSON.stringify({ "materials": {
-                    "albedo": [0, 0, 1]
-                  }}),
-                  lifetime: LIFETIME,
-                  priority: 1,
-                  parentMaterialName: 2,
-                  parentID: MyAvatar.SELF_ID
+        type: "Material",
+        materialURL: "materialData",
+        position: MyAvatar.position,
+        materialData: JSON.stringify({ "materials": {
+            "albedo": [0, 0, 1]
+        }}),
+        lifetime: LIFETIME,
+        priority: 1,
+        parentMaterialName: 2,
+        parentID: MyAvatar.SELF_ID
     }, true));
-    
+
+    var previousSkeletonURL;
+    var previousScale;
+    var prevCameraMode;
+
+    autoTester.addStep("Setup avatar", function () {
+        // Use a specific avatar.  This is needed because we want the avatar's height to be fixed.
+        previousSkeletonURL = MyAvatar.skeletonModelURL;
+        MyAvatar.skeletonModelURL = "https://highfidelity.com/api/v1/commerce/entity_edition/813addb9-b985-49c8-9912-36fdbb57e04a.fst?certificate_id=MEUCIQDgYR2%2BOrCh5HXeHCm%2BkR0a2JniEO%2BY4y9tbApxCAPo4wIgXZEQdI4cQc%2FstAcr9tFT9k4k%2Fbuj3ufB1aB4W0tjIJc%3D";
+
+        previousScale = MyAvatar.scale;
+        MyAvatar.scale = 1.0;
+
+        // Wait for skeleton to load (for now - only in test mode)
+        if (typeof Test !== 'undefined') {
+            Test.waitIdle();
+        }
+
+        // Set Avatar to T-pose
+        for (var i = 0; i < MyAvatar.getJointNames().length; ++i) {
+            MyAvatar.setJointData(i, MyAvatar.getDefaultJointRotation(i), MyAvatar.getDefaultJointTranslation(i));
+        }
+
+        prevCameraMode = Camera.mode;
+        Camera.mode = "mirror";
+    });    
     autoTester.addStepSnapshot("Avatar without material");
     
     autoTester.addStep("Add material to avatar", function () {
@@ -62,6 +81,11 @@ autoTester.perform("Apply Material Entities to Avatars", Script.resolvePath(".")
         for (var i = 0; i < createdEntities.length; i++) {
             Entities.deleteEntity(createdEntities[i]);
         }
+
+        // Restore avatar and camera mode
+        MyAvatar.skeletonModelURL = previousSkeletonURL;
+        MyAvatar.scale = previousScale;
+        MyAvatar.clearJointsData();
 
         Camera.mode = prevCameraMode;
     });
