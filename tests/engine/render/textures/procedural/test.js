aea1eb36af46241eeecc875a178770feabec16f5
NissimHadar <nissim.hadar@gmail.com>
2018-06-09 12:42:20 -0700

    Removed both KTX tests.

--- tests/engine/render/textures/procedural/test.js
+++ tests/engine/render/textures/procedural/test.js
@@ -5,8 +5,8 @@ if (typeof branch === 'undefined') branch = "master/";
 var autoTester = Script.require("https://github.com/" + user + repository + "blob/" + branch + "tests/utils/autoTester.js?raw=true" );
 
 var testFiles = [
-    { image: "uncompressed_color.ktx", resolution: [2048, 2048] },
-    { image: "compressed_color.ktx", resolution: [2048, 2048] },
+    ////{ image: "uncompressed_color.ktx", resolution: [2048, 2048] },
+    ////{ image: "compressed_color.ktx", resolution: [2048, 2048] },
     { image: "jpeg.jpg", resolution: [8192, 4096] },
 ];
 
@@ -22,8 +22,8 @@ autoTester.perform("Texture Rendering", Script.resolvePath("."), "secondary", fu
     });
 
     autoTester.addStep("Position avatar and camera", function() {
-        MyAvatar.position = Vec3.sum(MyAvatar.position, Vec3.multiplyQbyV(MyAvatar.orientation, { x: 0, y: 0.4, z: 0.2 }));
-        validationCamera_translate(Vec3.multiplyQbyV(MyAvatar.orientation, { x: 0, y: -0.2, z: 0.2 }));
+        MyAvatar.position = Vec3.sum(MyAvatar.position, Vec3.multiplyQbyV(MyAvatar.orientation, { x: 0, y: 0.4, z: 0.0 }));
+        validationCamera_translate(Vec3.multiplyQbyV(MyAvatar.orientation, { x: 0, y: -0.2, z: 0.0 }));
     });
 
     testFiles.forEach(function(props) {
