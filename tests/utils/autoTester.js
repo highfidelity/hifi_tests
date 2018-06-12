17b6fe90d9511bc26265f8c84844d4b33f1cdc78
NissimHadar <nissim.hadar@gmail.com>
2018-06-12 12:26:30 -0700

    Added use of `getOriginFrame`.

--- tests/utils/autoTester.js
+++ tests/utils/autoTester.js
@@ -406,6 +406,6 @@ module.exports.runRecursive = function () {
 }
 
 // This is a position; the orientation is assumed to be looking down the Z axis, Y is up.
-module.exports.getOriginFrame() {
+module.exports.getOriginFrame() = function () {
     return originFrame;
 }
