59b67a4aa7c9e88f4397ab5a3966a9d10c8c2948
NissimHadar <nissim.hadar@gmail.com>
2018-06-13 12:20:01 -0700

    Added missing START /WAIT Added delay after starting server

--- batch files/DailyTests.bat
+++ batch files/DailyTests.bat
@@ -24,9 +24,12 @@ START /WAIT HighFidelity-Beta-latest-dev.exe /S /D=%INSTALL_DIR%
 ECHO Starting local server
 start "DS" %INSTALL_DIR%\domain-server.exe
 start "AC" %INSTALL_DIR%\assignment-client.exe -n 6
- 
+
+ECHO Waiting for server to stabilize 
+ping localhost -n 7 >nul
+
 ECHO Running Interface tests
-%INSTALL_DIR%\interface.exe --url hifi://localhost/8000,8000,8000/0,0.0,0.0,1.0 --testScript https://raw.githubusercontent.com/highfidelity/hifi_tests/%BRANCH%/tests/testRecursive.js quitWhenFinished --testResultsLocation %TEST_RESULT_LOCATION%
+START /WAIT %INSTALL_DIR%\interface.exe --url hifi://localhost/8000,8000,8000/0,0.0,0.0,1.0 --testScript https://raw.githubusercontent.com/highfidelity/hifi_tests/%BRANCH%/tests/testRecursive.js quitWhenFinished --testResultsLocation %TEST_RESULT_LOCATION%
 
 ECHO Stopping local server
 taskkill /im assignment-client.exe /f >nul
