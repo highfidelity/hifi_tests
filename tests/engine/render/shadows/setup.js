25ae4d77cc7570f4098c2248a0717829c625e9df
NissimHadar <nissim.hadar@gmail.com>
2018-06-14 07:28:16 -0700

    Only define repository parameters if not previously defined.

--- tests/engine/render/shadows/setup.js
+++ tests/engine/render/shadows/setup.js
@@ -8,9 +8,9 @@
 // Test material matrix
 Script.include("../../../utils/test_stage.js?raw=true")
 
-var user = "highfidelity/";
-var repository = "hifi_tests/";
-var branch = "master/";
+if (typeof user === 'undefined') user = "highfidelity/";
+if (typeof repository === 'undefined') repository = "hifi_tests/";
+if (typeof branch === 'undefined') branch = "master/";
 
 var TESTS_URL = "https://github.com/" + user + repository + "blob/" + branch;
 var RAW_TESTS_URL = "https://raw.githubusercontent.com/" + user + repository + branch;
