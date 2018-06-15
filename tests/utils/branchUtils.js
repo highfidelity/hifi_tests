4799530883441865d7274d27a011b5299a08a20a
NissimHadar <nissim.hadar@gmail.com>
2018-06-14 18:45:39 -0700

    Debug.

--- tests/utils/branchUtils.js
+++ tests/utils/branchUtils.js
@@ -2,9 +2,13 @@
 getBranch = function (path, repository) {
     // The branch is the word after the repository name, and before the word "tests"
     // Assumes that the branch exists in the path
+    
+    // Note that the repository includes a final '/'
+    var repositoryName = repository.slice(0,-1);
+    
     var words = path.split("/");
     for (var i = 0; i < words.length - 2; ++i) {
-        if (words[i] === repository && words[i + 2] === "tests") {
+        if (words[i] === repositoryName && words[i + 2] === "tests") {
             return words[i + 1];
         }
     }
