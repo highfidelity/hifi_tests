7dafed8760740c7502c8f3998b8ad4fb12489786
NissimHadar <nissim.hadar@gmail.com>
2018-06-15 12:38:11 -0700

    First test.

--- tests/content/entity/light/point/create/test.js
+++ tests/content/entity/light/point/create/test.js
@@ -1,7 +1,7 @@
 if (typeof user === 'undefined') user = "highfidelity/";
 if (typeof repository === 'undefined') repository = "hifi_tests/";
 
-Script.include("https://raw.githubusercontent.com/NissimHadar/hifi_tests/addRCtoDailyTests/tests/utils/branchUtils.js");
+Script.include("https://github.com/NissimHadar/hifi_tests/blob/addRCtoDailyTests/tests/utils/branchUtils.js?raw=true");
 if (typeof branch === 'undefined') branch = getBranch(Script.resolvePath("."), repository) +"/";
 
 var autoTester = Script.require("https://github.com/" + user + repository + "blob/" + branch + "tests/utils/autoTester.js?raw=true" );
