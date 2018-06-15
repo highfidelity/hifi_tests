4e9264d3a65fab3084c78af465325fc9083d2ad9
NissimHadar <nissim.hadar@gmail.com>
2018-06-15 08:24:34 -0700

    Deal with call from local script.

--- tests/utils/branchUtils.js
+++ tests/utils/branchUtils.js
@@ -4,14 +4,22 @@ getBranch = function (path, repository) {
     // Assumes that the branch exists in the path
 
     // Note that the repository includes a final '/'
-    var repositoryName = repository.slice(0, -1);
-    
+    var repositoryName = repository.slice(0,-1);
+
     var words = path.split("/");
+
+    // If the first word is not "https" then the script is probably running from a local file
+    // In this case, assume the required branch is "master"
+    if (words[0] !== "htpps") {
+        return "master";
+    }
+
     for (var i = 0; i < words.length - 2; ++i) {
         if (words[i] === repositoryName && words[i + 2] === "tests") {
             return words[i + 1];
         }
     }
 
+    // This should never happen
     return "branch not found";
 }
