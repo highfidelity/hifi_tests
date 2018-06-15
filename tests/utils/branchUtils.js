673013d980c090badcb54ceb4fa195d04ba04d9c
NissimHadar <nissim.hadar@gmail.com>
2018-06-14 18:49:52 -0700

    getBranch() method

---
+++ tests/utils/branchUtils.js
@@ -0,0 +1,17 @@
+// Find the GitHub branch we are running from
+getBranch = function (path, repository) {
+    // The branch is the word after the repository name, and before the word "tests"
+    // Assumes that the branch exists in the path
+    
+    // Note that the repository includes a final '/'
+    var repositoryName = repository.slice(0,-1);
+    
+    var words = path.split("/");
+    for (var i = 0; i < words.length - 2; ++i) {
+        if (words[i] === repositoryName && words[i + 2] === "tests") {
+            return words[i + 1];
+        }
+    }
+    
+    return "branch not found";
+}
