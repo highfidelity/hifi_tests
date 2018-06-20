6c85783ec2a9b8c0c25e65f1c85b36a8806d24be
NissimHadar <nissim.hadar@gmail.com>
2018-06-20 13:57:30 -0700

    Rest of the tests.

--- tests/engine/render/lighting/ponctual/onTransparent/test.js
+++ tests/engine/render/lighting/ponctual/onTransparent/test.js
@@ -1,10 +1,5 @@
-if (typeof user === 'undefined') user = "highfidelity/";
-if (typeof repository === 'undefined') repository = "hifi_tests/";
-
 Script.include("https://github.com/highfidelity/hifi_tests/blob/RC69/tests/utils/branchUtils.js?raw=true");
-if (typeof branch === 'undefined') branch = getBranch(Script.resolvePath("."), repository) +"/";
-
-var autoTester = Script.require("https://github.com/" + user + repository + "blob/" + branch + "tests/utils/autoTester.js?raw=true" );
+var autoTester = createAutoTester(Script.resolvePath("."));
 
 autoTester.perform("Lighting on Transparent Object", Script.resolvePath("."), "secondary", function(testType) {
     // Test material matrix
