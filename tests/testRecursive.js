6ce914a5d0ecb950e7d70e9177d8525979e9f729
NissimHadar <nissim.hadar@gmail.com>
2018-06-20 14:05:46 -0700

    Testing.

--- tests/testRecursive.js
+++ tests/testRecursive.js
@@ -1,17 +1,11 @@
 // This is an automatically generated file, created by auto-tester on Jun 15 2018, 14:39
 
-user = "highfidelity/";
-repository = "hifi_tests/";
-
 Script.include("https://github.com/highfidelity/hifi_tests/blob/RC69/tests/utils/branchUtils.js?raw=true");
-branch = getBranch(Script.resolvePath("."), repository) +"/";
+var autoTester = createAutoTester(Script.resolvePath("."));
 
 if (typeof Test !== 'undefined') {
     Test.wait(10000);
 };
-
-var repositoryPath = "https://github.com/" + user + repository + "blob/" + branch;
-var autoTester = Script.require(repositoryPath + "tests/utils/autoTester.js?raw=true");
 
 autoTester.enableRecursive();
 autoTester.enableAuto();
