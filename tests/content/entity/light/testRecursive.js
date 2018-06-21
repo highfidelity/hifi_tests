4da53fa276efcc6bd5edba557d2005d1e0c2d159
NissimHadar <nissim.hadar@gmail.com>
2018-06-21 16:28:07 -0700

    Debug.

--- tests/content/entity/light/testRecursive.js
+++ tests/content/entity/light/testRecursive.js
@@ -1,6 +1,6 @@
 // This is an automatically generated file, created by auto-tester on Jun 21 2018, 14:25
 
-Script.include("https://raw.githubusercontent.com/highfidelity/hifi_tests/master/tests/utils/branchUtils.js");
+Script.include("https://raw.githubusercontent.com/NissimHadar/hifi_tests/addRCtoDailyTests/tests/utils/branchUtils.js");
 var autoTester = createAutoTester(Script.resolvePath("."));
 
 var repositoryPath = autoTester.getRepositoryPath();
