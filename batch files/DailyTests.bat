9c69715255a125a5a9e2d98f6e591593e8244e7c
NissimHadar <nissim.hadar@gmail.com>
2018-06-22 11:06:42 -0700

    Adding USER parameter.

--- batch files/DailyTests.bat
+++ batch files/DailyTests.bat
@@ -1,13 +1,22 @@
+REM First parameter is compulsory - which GitHub branch to run from
+REM Second parameter selects the GitHub user, default is "highfidelity"
+
 @ECHO OFF
 REM Exit, with message, if branch parameter is missing
-IF NOT "%~1" == "" GOTO :OK
+IF NOT "%~1" == "" GOTO :BRANCH_PARAMETER_FOUND
 ECHO Missing branch parameter
 EXIT /b
 
-:OK
+:BRANCH_PARAMETER_FOUND
     
 SET BRANCH=%1
 ECHO Starting Test for %BRANCH%
+
+SET USER="highfidelity"
+IF NOT "%~2" == "" GOTO :NO_USER_PARAMETER
+SET USER=%2
+
+:NO_USER_PARAMETER
 
 ECHO Before starting - stop any local server that may be running
 taskkill /im assignment-client.exe /f >nul
@@ -43,12 +52,12 @@ ECHO Waiting for server to stabilize
 ping localhost -n 7 >nul
 
 ECHO Running Interface tests
-START /WAIT %INSTALL_DIR%\interface.exe --url hifi://localhost/8000,8000,8000/0,0.0,0.0,1.0 --testScript https://raw.githubusercontent.com/highfidelity/hifi_tests/%BRANCH%/tests/testRecursive.js quitWhenFinished --testResultsLocation %TEST_RESULT_LOCATION%
+START /WAIT %INSTALL_DIR%\interface.exe --url hifi://localhost/8000,8000,8000/0,0.0,0.0,1.0 --testScript https://raw.githubusercontent.com/%USER%/"hifi_tests"/%BRANCH%/tests/testRecursive.js quitWhenFinished --testResultsLocation %TEST_RESULT_LOCATION%
 
 ECHO Stopping local server
 taskkill /im assignment-client.exe /f >nul
 taskkill /im domain-server.exe /f >nul
 
 ECHO Completed test, starting evaluation
-START /WAIT %AUTOTESTER_PATH%\Release\auto-tester.exe --testFolder %TEST_RESULT_LOCATION% --branch %BRANCH%
+START /WAIT %AUTOTESTER_PATH%\Release\auto-tester.exe --testFolder %TEST_RESULT_LOCATION% --branch %BRANCH% --user %USER%
 ECHO Evaluation complete
