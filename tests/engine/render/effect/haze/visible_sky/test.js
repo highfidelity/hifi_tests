161995ba58fc166127d70a95ef93887f0e653ffe
NissimHadar <nissim.hadar@gmail.com>
2018-06-23 10:13:58 -0700

    Enable working from any repository.

--- tests/engine/render/effect/haze/visible_sky/test.js
+++ tests/engine/render/effect/haze/visible_sky/test.js
@@ -1,4 +1,5 @@
-Script.include("https://raw.githubusercontent.com/highfidelity/hifi_tests/master/tests/utils/branchUtils.js");
+if (PATH_TO_THE_REPO_PATH_UTILS_FILE === undefined) PATH_TO_THE_REPO_PATH_UTILS_FILE = "https://raw.githubusercontent.com/highfidelity/hifi_tests/master/tests/utils/branchUtils.js"
+Script.include(PATH_TO_THE_REPO_PATH_UTILS_FILE);
 var autoTester = createAutoTester(Script.resolvePath("."));
 
 autoTester.perform("Haze - completely visible sky", Script.resolvePath("."), "secondary", function(testType) {
