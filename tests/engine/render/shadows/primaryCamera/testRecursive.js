6804dc463309a1762d8b5e1353bc69d070e95408
NissimHadar <nissim.hadar@gmail.com>
2018-06-25 17:46:53 -0700

    Update scripts per new API.

---
+++ tests/engine/render/shadows/primaryCamera/testRecursive.js
@@ -0,0 +1,20 @@
+// This is an automatically generated file, created by auto-tester on Jun 24 2018, 12:31
+
+PATH_TO_THE_REPO_PATH_UTILS_FILE = "https://raw.githubusercontent.com/highfidelity/hifi_tests/master/tests/utils/branchUtils.js";
+Script.include(PATH_TO_THE_REPO_PATH_UTILS_FILE);
+var autoTester = createAutoTester(Script.resolvePath("."));
+
+var testsRootPath = autoTester.getTestsRootPath();
+
+if (typeof Test !== 'undefined') {
+    Test.wait(10000);
+};
+
+autoTester.enableRecursive();
+autoTester.enableAuto();
+
+Script.include(testsRootPath + "engine/render/shadows/primaryCamera/normal/test.js");
+Script.include(testsRootPath + "engine/render/shadows/primaryCamera/grazing/test.js");
+Script.include(testsRootPath + "engine/render/shadows/primaryCamera/front/test.js");
+
+autoTester.runRecursive();
---
+++ tests/engine/render/shadows/secondaryCamera/testRecursive.js
@@ -0,0 +1,17 @@
+// This is an automatically generated file, created by auto-tester on Jun 24 2018, 12:31
+
+PATH_TO_THE_REPO_PATH_UTILS_FILE = "https://raw.githubusercontent.com/highfidelity/hifi_tests/master/tests/utils/branchUtils.js";
+Script.include(PATH_TO_THE_REPO_PATH_UTILS_FILE);
+var autoTester = createAutoTester(Script.resolvePath("."));
+
+var testsRootPath = autoTester.getTestsRootPath();
+
+if (typeof Test !== 'undefined') {
+    Test.wait(10000);
+};
+
+autoTester.enableRecursive();
+autoTester.enableAuto();
+
+
+autoTester.runRecursive();
