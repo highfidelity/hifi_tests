fb9558cc2c1b957ac1d596e7b54cb185db7d26d2
NissimHadar <nissim.hadar@gmail.com>
2018-06-25 17:45:03 -0700

    Update scripts per new API.

---
+++ tests/engine/render/shadows/secondaryCamera/normal/testStory.js
@@ -0,0 +1,24 @@
+if (typeof PATH_TO_THE_REPO_PATH_UTILS_FILE === 'undefined') PATH_TO_THE_REPO_PATH_UTILS_FILE = "https://raw.githubusercontent.com/highfidelity/hifi_tests/master/tests/utils/branchUtils.js";
+Script.include(PATH_TO_THE_REPO_PATH_UTILS_FILE);
+var autoTester = createAutoTester(Script.resolvePath("."));
+
+autoTester.perform("Shadow - light on top", Script.resolvePath("."), "secondary", function(testType) {
+    // Test material matrix
+    Script.include("../../setup.js?raw=true")
+
+    // Add the test Cases
+    var createdEntities = [];
+    autoTester.addStep("Set up test case", function () {
+        createdEntities = setup(80.0, -60.0, autoTester.getOriginFrame());
+    });
+
+    autoTester.addStepSnapshot("Light source altitude: 80.0, azimuth: -60.0");
+
+    autoTester.addStep("Clean up after test", function () {
+        for (var i = 0; i < createdEntities.length; i++) {
+            Entities.deleteEntity(createdEntities[i]);
+        }
+    });
+
+    var result = autoTester.runTest(testType);
+});
