0d8ffb6151c9b1e3942a77cb008a86586ac39218
NissimHadar <nissim.hadar@gmail.com>
2018-07-05 10:26:46 -0700

    Adding overlay recursive tests.

---
+++ tests/content/overlay/testRecursive.js
@@ -0,0 +1,21 @@
+// This is an automatically generated file, created by auto-tester on Jul 5 2018, 10:19
+
+PATH_TO_THE_REPO_PATH_UTILS_FILE = "https://raw.githubusercontent.com/highfidelity/hifi_tests/master/tests/utils/branchUtils.js";
+Script.include(PATH_TO_THE_REPO_PATH_UTILS_FILE);
+var autoTester = createAutoTester(Script.resolvePath("."));
+
+var testsRootPath = autoTester.getTestsRootPath();
+
+if (typeof Test !== 'undefined') {
+    Test.wait(10000);
+};
+
+autoTester.enableRecursive();
+autoTester.enableAuto();
+
+Script.include(testsRootPath + "content/overlay/material/test.js");
+Script.include(testsRootPath + "content/overlay/layer/drawInFront/shape/test.js");
+Script.include(testsRootPath + "content/overlay/layer/drawInFront/model/test.js");
+Script.include(testsRootPath + "content/overlay/layer/drawHUDLayer/test.js");
+
+autoTester.runRecursive();
