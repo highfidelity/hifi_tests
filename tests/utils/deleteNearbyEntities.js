12c8cac0a378f7a9a095bfdb1b355d30d10c48f7
NissimHadar <nissim.hadar@gmail.com>
2018-12-27 10:25:11 -0800

    Updated comment.

--- tests/utils/deleteNearbyEntities.js
+++ tests/utils/deleteNearbyEntities.js
@@ -1,4 +1,4 @@
-// Remove all entities near the avatar
+// Remove all entities near (i.e. with 1000m from) the avatar
 const TEST_RADIUS = 1000;
 var entitiesToDelete = Entities.findEntities(MyAvatar.position, TEST_RADIUS);
 for (var i = 0; i < entitiesToDelete.length; ++i) {
