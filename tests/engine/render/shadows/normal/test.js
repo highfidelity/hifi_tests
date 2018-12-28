f8a5f4619d0defe1709b0dd1e74836ab13a60832
NissimHadar <nissim.hadar@gmail.com>
2018-12-27 16:47:29 -0800

    Use secondary camera for shadow tests.

--- tests/engine/render/shadows/normal/test.js
+++ tests/engine/render/shadows/normal/test.js
@@ -2,18 +2,18 @@ if (typeof PATH_TO_THE_REPO_PATH_UTILS_FILE === 'undefined') PATH_TO_THE_REPO_PA
 Script.include(PATH_TO_THE_REPO_PATH_UTILS_FILE);
 var nitpick = createNitpick(Script.resolvePath("."));
 
-nitpick.perform("Shadow - light in front", Script.resolvePath("."), "primary", function(testType) {
+nitpick.perform("Shadow - light on top", Script.resolvePath("."), "secondary", function(testType) {
     // Test material matrix
-    Script.include("../../setup.js?raw=true")
+    Script.include("../setup.js?raw=true");
 
     // Add the test Cases
     var createdEntities = [];
     nitpick.addStep("Set up test case", function () {
-        createdEntities = setup(20.0, 180.0, nitpick.getOriginFrame());
+        createdEntities = setup(80.0, -60.0, nitpick.getOriginFrame());
     });
 
     nitpick.addDelay(3);
-    nitpick.addStepSnapshot("Light source altitude: 20.0, azimuth: 180.0");
+    nitpick.addStepSnapshot("Light source altitude: 80.0, azimuth: -60.0");
 
     nitpick.addStep("Clean up after test", function () {
         for (var i = 0; i < createdEntities.length; i++) {
