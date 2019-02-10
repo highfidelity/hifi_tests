fa472689c3b0b2e35bc449ab585ddf271323b9d1
NissimHadar <nissim.hadar@gmail.com>
2019-02-10 10:42:11 -0800

    Updated for new recursive scripts.

--- tests/content/entity/model/modelReaders/gltfReader/gltfTestSuite/embedded/testRecursive.js
+++ tests/content/entity/model/modelReaders/gltfReader/gltfTestSuite/embedded/testRecursive.js
@@ -2,14 +2,13 @@
 PATH_TO_THE_REPO_PATH_UTILS_FILE = "https://raw.githubusercontent.com/highfidelity/hifi_tests/master/tests/utils/branchUtils.js";
 Script.include(PATH_TO_THE_REPO_PATH_UTILS_FILE);
 
-if (typeof nitpick === 'undefined') var nitpick = createNitpick(Script.resolvePath("."));
-if (typeof testsRootPath === 'undefined') var testsRootPath = nitpick.getTestsRootPath();
+if (typeof nitpick === 'undefined') nitpick = createNitpick(Script.resolvePath("."));
+if (typeof testsRootPath === 'undefined') testsRootPath = nitpick.getTestsRootPath();
 
 nitpick.enableRecursive();
 nitpick.enableAuto();
 
 Script.include(testsRootPath + "content/entity/model/modelReaders/gltfReader/gltfTestSuite/embedded/vertexColor/test.js");
-Script.include(testsRootPath + "content/entity/model/modelReaders/gltfReader/gltfTestSuite/embedded/triangleNoIndices/test.js");
 Script.include(testsRootPath + "content/entity/model/modelReaders/gltfReader/gltfTestSuite/embedded/triangle/test.js");
 Script.include(testsRootPath + "content/entity/model/modelReaders/gltfReader/gltfTestSuite/embedded/textureSettings/test.js");
 Script.include(testsRootPath + "content/entity/model/modelReaders/gltfReader/gltfTestSuite/embedded/sparseAccessor/test.js");
--- tests/content/entity/shape/testRecursive.js
+++ tests/content/entity/shape/testRecursive.js
@@ -2,8 +2,8 @@
 PATH_TO_THE_REPO_PATH_UTILS_FILE = "https://raw.githubusercontent.com/highfidelity/hifi_tests/master/tests/utils/branchUtils.js";
 Script.include(PATH_TO_THE_REPO_PATH_UTILS_FILE);
 
-if (typeof nitpick === 'undefined') var nitpick = createNitpick(Script.resolvePath("."));
-if (typeof testsRootPath === 'undefined') var testsRootPath = nitpick.getTestsRootPath();
+if (typeof nitpick === 'undefined') nitpick = createNitpick(Script.resolvePath("."));
+if (typeof testsRootPath === 'undefined') testsRootPath = nitpick.getTestsRootPath();
 
 nitpick.enableRecursive();
 nitpick.enableAuto();
