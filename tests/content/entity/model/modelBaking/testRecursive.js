2ec9a5a6ce9279d8ddad7464d575d2a41190bdba
NissimHadar <nissim.hadar@gmail.com>
2019-02-10 10:44:10 -0800

    Added missing recursive script.

---
+++ tests/content/entity/model/modelBaking/testRecursive.js
@@ -0,0 +1,13 @@
+// This is an automatically generated file, created by nitpick
+PATH_TO_THE_REPO_PATH_UTILS_FILE = "https://raw.githubusercontent.com/highfidelity/hifi_tests/master/tests/utils/branchUtils.js";
+Script.include(PATH_TO_THE_REPO_PATH_UTILS_FILE);
+
+if (typeof nitpick === 'undefined') nitpick = createNitpick(Script.resolvePath("."));
+if (typeof testsRootPath === 'undefined') testsRootPath = nitpick.getTestsRootPath();
+
+nitpick.enableRecursive();
+nitpick.enableAuto();
+
+Script.include(testsRootPath + "content/entity/model/modelBaking/normal_texture/test.js");
+
+nitpick.runRecursive();
