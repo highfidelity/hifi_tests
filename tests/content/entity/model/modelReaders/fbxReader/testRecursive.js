6a69951068db5eb561b7833586d035e67c15c0aa
NissimHadar <nissim.hadar@gmail.com>
2019-02-11 12:18:33 -0800

    Updated recursive scripts.

--- tests/content/entity/model/modelReaders/fbxReader/testRecursive.js
+++ tests/content/entity/model/modelReaders/fbxReader/testRecursive.js
@@ -1,18 +1,19 @@
 // This is an automatically generated file, created by nitpick
 PATH_TO_THE_REPO_PATH_UTILS_FILE = "https://raw.githubusercontent.com/highfidelity/hifi_tests/master/tests/utils/branchUtils.js";
 Script.include(PATH_TO_THE_REPO_PATH_UTILS_FILE);
-var nitpick = createNitpick(Script.resolvePath("."));
 
-var testsRootPath = nitpick.getTestsRootPath();
-
-if (typeof Test !== 'undefined') {
-    Test.wait(10000);
-};
+if (typeof nitpick === 'undefined') nitpick = createNitpick(Script.resolvePath("."));
+if (typeof testsRootPath === 'undefined') testsRootPath = nitpick.getTestsRootPath();
 
 nitpick.enableRecursive();
 nitpick.enableAuto();
 
+Script.include(testsRootPath + "content/entity/model/modelReaders/fbxReader/uv_scale/test.js");
 Script.include(testsRootPath + "content/entity/model/modelReaders/fbxReader/still_life/test.js");
 Script.include(testsRootPath + "content/entity/model/modelReaders/fbxReader/simple/test.js");
 
-nitpick.runRecursive();
+if (typeof runningRecursive === 'undefined') {
+   runningRecursive = true;
+   nitpick.runRecursive();
+}
+
