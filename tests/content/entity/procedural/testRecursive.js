6e3ba3fe301a1adcb020129e3584b812b679ce3a
NissimHadar <nissim.hadar@gmail.com>
2019-02-27 14:41:51 -0800

    Updated all recursive scripts.

--- tests/content/entity/procedural/testRecursive.js
+++ tests/content/entity/procedural/testRecursive.js
@@ -10,8 +10,8 @@ if (typeof depth === 'undefined') {
    nitpick.enableRecursive();
    nitpick.enableAuto();
 
-   if (typeof TestCreator !== 'undefined') {
-       TestCreator.wait(10000);
+   if (typeof Test !== 'undefined') {
+       Test.wait(10000);
    }
 } else {
    depth++
--- tests/content/entity/testRecursive.js
+++ tests/content/entity/testRecursive.js
@@ -10,8 +10,8 @@ if (typeof depth === 'undefined') {
    nitpick.enableRecursive();
    nitpick.enableAuto();
 
-   if (typeof TestCreator !== 'undefined') {
-       TestCreator.wait(10000);
+   if (typeof Test !== 'undefined') {
+       Test.wait(10000);
    }
 } else {
    depth++
@@ -34,3 +34,4 @@ if (depth > 0) {
 } else {
    nitpick.runRecursive();
 }
+
