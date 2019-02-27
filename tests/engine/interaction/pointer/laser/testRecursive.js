4c43232f8acba53a193813b85ee20f8d041c70d3
NissimHadar <nissim.hadar@gmail.com>
2019-02-27 14:07:07 -0800

    Updated most recursive scripts.

--- tests/engine/interaction/pointer/laser/testRecursive.js
+++ tests/engine/interaction/pointer/laser/testRecursive.js
@@ -9,6 +9,10 @@ if (typeof depth === 'undefined') {
 
    nitpick.enableRecursive();
    nitpick.enableAuto();
+
+   if (typeof TestCreator !== 'undefined') {
+       TestCreator.wait(10000);
+   }
 } else {
    depth++
 }
