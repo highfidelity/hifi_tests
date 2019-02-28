6fd2754477d40663e19159d64c96b38bfcbe2c00
NissimHadar <nissim.hadar@gmail.com>
2019-02-28 11:59:50 -0800

    Reverted unneeded changes.

--- tests/content/entity/shape/testRecursive.js
+++ tests/content/entity/shape/testRecursive.js
@@ -9,10 +9,6 @@ if (typeof depth === 'undefined') {
 
    nitpick.enableRecursive();
    nitpick.enableAuto();
-
-   if (typeof Test !== 'undefined') {
-       Test.wait(10000);
-   }
 } else {
    depth++
 }
