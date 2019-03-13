dc814746ece22a45ec9bfc3cfb28f941477e709f
amerhifi <43353902+amerhifi@users.noreply.github.com>
2019-03-13 14:02:38 -0700

    Moving pad and format date functions
    Moving functions to prevent re-def of functions since they share the same name with other js files.

--- tests/performance/manualTrace/testHmdManual.js
+++ tests/performance/manualTrace/testHmdManual.js
@@ -5,33 +5,32 @@ DEFAULT_TRACING_RULES = "" +
 
 // Creates a string of length 'size' from the number 'num'
 // the resulting string is left-padded as needed with '0'
-function pad(num, size) {
-    var s = num + "";
-    while (s.length < size) {
-        s = "0" + s; 
-    }
-    
-    return s;
-}
 
-// Returns the current data and time as a string in the following format:
-//    yyyymmdd_hhmmss
-function formatDate(date) {
-    date = new Date();
-    return date.getFullYear() + pad(date.getMonth() + 1, 2) + pad(date.getDate(), 2) + "_" + 
-    pad(date.getHours(), 2) + pad(date.getMinutes(), 2) + pad(date.getSeconds(), 2);
-}
 
 var count=0;
 (function(){
 	var tablet = Tablet.getTablet("com.highfidelity.interface.tablet.system");
 	
-	var buttonStart = tablet.addButton({
-    text: "Start Trace"});
+	var buttonStart = tablet.addButton({text: "Start Trace"});
 
-	var buttonStop = tablet.addButton({
-    text: "Stop Trace"});
+	var buttonStop = tablet.addButton({text: "Stop Trace"});
 	
+    	function pad(num, size) {
+        	var s = num + "";
+        	while (s.length < size) {
+         	   	s = "0" + s; 
+        	}
+         return s;
+    	}
+
+    	// Returns the current data and time as a string in the following format:
+    	// yyyymmdd_hhmmss
+    	function formatDate(date) {
+        	date = new Date();
+        	return date.getFullYear() + pad(date.getMonth() + 1, 2) + pad(date.getDate(), 2) + "_" + 
+        	pad(date.getHours(), 2) + pad(date.getMinutes(), 2) + pad(date.getSeconds(), 2);
+    	}
+
 	buttonStart.clicked.connect(function () {
 		Window.displayAnnouncement("Starting test - please wait");
 		Test.startTracing(DEFAULT_TRACING_RULES);
