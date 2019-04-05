e68b35e83531abbf509cf0530d2d6b656ca1014b
raveenajain <jain.rav@husky.neu.edu>
2019-04-05 21:58:07 +0100

    position, image update

--- tests/content/entity/model/modelReaders/gltfReader/gltfTestSuite/embedded/brainstem/test.js
+++ tests/content/entity/model/modelReaders/gltfReader/gltfTestSuite/embedded/brainstem/test.js
@@ -26,7 +26,7 @@ nitpick.perform("Read GLTF model", Script.resolvePath("."), "secondary", functio
         type: "Model",
         // https://github.com/KhronosGroup/glTF-Sample-Models/tree/master/2.0/BrainStem
         modelURL: 'https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/BrainStem/glTF-Embedded/BrainStem.gltf',
-        position: Vec3.sum(position, {x: 0.0, y: 0.3, z: -3.2 }),
+        position: Vec3.sum(position, {x: 0.0, y: 0.0, z: -3.2 }),
         rotation: Quat.fromPitchYawRollDegrees(0.0, 0.0, 0.0),
         visible: true,
         userData: JSON.stringify({ grabbableKey: { grabbable: false } })
