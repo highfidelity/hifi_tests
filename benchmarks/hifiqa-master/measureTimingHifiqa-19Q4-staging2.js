cb3be69678c3f5971ace63d768f16ad40c68b9d6
Adam Smith <41648433+qaAdamSmith82@users.noreply.github.com>
2019-09-16 14:32:02 -0700

    Update measureTimingHifiqa-19Q4-staging2.js

--- benchmarks/hifiqa-master/measureTimingHifiqa-19Q4-staging2.js
+++ benchmarks/hifiqa-master/measureTimingHifiqa-19Q4-staging2.js
@@ -8,15 +8,15 @@ var TRACING_RULES = "" +
 Resources.overrideUrlPrefix(TEST_ROOT, Script.resolvePath(".."));
 var testScript = new TestScript();
 testScript.addTest({
-    name: "measureTimingHifiqa-19Q4-staging2",
+    name: "measureTimingHifiqa-19Q4-staging1",
     loader: TestScript.locationLoader("hifi://staging2", true),
     tracingRules: TRACING_RULES,
     traceActions: TestScript.measureTimingSteps([
-        { time: 5, step: 0.1, keepActive: true, pos: { x:   81.5, y: -97.2, z: -312.8 }, ori:{ yaw:  127.1 } }, // landing point
-        { time: 5, step: 0.1, keepActive: true, pos: { x:   154.8, y: -97.7, z: -395.6 }, ori:{ yaw:   90.9 } }, // looking at quad
-        { time: 5, step: 0.1, keepActive: true, pos: { x:  159.6, y: -96.8, z:  -325.6 }, ori:{ yaw: -125.2 } }, // engine pod
-        { time: 5, step: 0.1, keepActive: true, pos: { x:  127.3, y: -91.8, z: -336.2 }, ori:{ yaw:  173.6 } }, // near a chatterbox
-        { time: 5, step: 0.1, keepActive: true, pos: { x: 119.0, y: -93.9, z:  -401.8 }, ori:{ yaw:   40.7 } }, // capitol
+        { time: 5, step: 0.1, keepActive: true, pos: { x:   81.5, y: -97.2, z: -312.8 }, ori:{ yaw:  -45.6 } }, 
+        { time: 5, step: 0.1, keepActive: true, pos: { x:   154.8, y: -97.7, z: -395.6 }, ori:{ yaw:   167.5 } }, 
+        { time: 5, step: 0.1, keepActive: true, pos: { x:  159.6, y: -96.8, z:  -325.6 }, ori:{ yaw: 32.5 } }, 
+        { time: 5, step: 0.1, keepActive: true, pos: { x:  127.3, y: -91.8, z: -336.2 }, ori:{ yaw:  15.7 } }, 
+        { time: 5, step: 0.1, keepActive: true, pos: { x: 119.0, y: -93.9, z:  -401.8 }, ori:{ yaw:   126.5 } }, 
     ]),
     duration: 30
 });
