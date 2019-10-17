695bf232efe2b2cd05244ea2159db39ee14029ee
Andrew Meadows <andrew@highfidelity.io>
2019-10-17 10:14:41 -0700

    don't delete domain entity children of avatar entities

--- tests/protocol/delete/test.js
+++ tests/protocol/delete/test.js
@@ -33,36 +33,36 @@ nitpick.perform("Delete protocol sanity - TEST REQUIRES SERVER", Script.resolveP
         lifetime: 600
     };
 
-    nitpick.addStep("Create parentEntity", function () {
+    nitpick.addStep("Create AVATAR parentEntity", function () {
         parentEntity = Entities.addEntity(parentProperties, "avatar");
     });
-    nitpick.addStep("Verify parentEntity", function () {
+    nitpick.addStep("Verify AVATAR parentEntity", function () {
         var getProperties = Entities.getEntityProperties(parentEntity);
         saveResults(compareObjects(parentProperties, getProperties));
     });
 
-    nitpick.addStep("Create childEntity1 of 3", function () {
+    nitpick.addStep("Create DOMAIN childEntity1 of 3", function () {
         childEntityProperties.name = "childEntity1";
         childEntityProperties.parentID = parentEntity;
         childEntity1 = Entities.addEntity(childEntityProperties, "domain");
     });
-    nitpick.addStep("Verify childEntity1", function () {
+    nitpick.addStep("Verify DOMAIN childEntity1", function () {
         var getProperties = Entities.getEntityProperties(childEntity1);
         saveResults(compareObjects(childEntityProperties, getProperties));
     });
 
-    nitpick.addStep("Create childEntity2 of 3", function () {
+    nitpick.addStep("Create AVATAR childEntity2 of 3", function () {
         childEntityProperties.name = "childEntity2";
         childEntityProperties.parentID = childEntity1;
         childEntityProperties.color = { red: 0, green: 0, blue: 255 };
         childEntity2 = Entities.addEntity(childEntityProperties, "avatar");
     });
-    nitpick.addStep("Verify childEntity2", function () {
+    nitpick.addStep("Verify AVATAR childEntity2", function () {
         var getProperties = Entities.getEntityProperties(childEntity2);
         saveResults(compareObjects(childEntityProperties, getProperties));
     });
 
-    nitpick.addStep("Create childEntity3 of 3", function () {
+    nitpick.addStep("Create DOMAIN childEntity3 of 3", function () {
         childEntityProperties.name = "childEntity3";
         childEntityProperties.parentID = childEntity2;
         childEntityProperties.color = { red: 0, green: 255, blue: 255 };
@@ -73,11 +73,11 @@ nitpick.perform("Delete protocol sanity - TEST REQUIRES SERVER", Script.resolveP
         saveResults(compareObjects(childEntityProperties, getProperties));
     });
 
-    nitpick.addStep("Delete childEntity1 --> all subsequent children should be deleted", function () {
+    nitpick.addStep("Delete DOMAIN childEntity1 --> AVATAR childEntity2 should be deleted, but not DOMAIN childEntity3", function () {
         Entities.deleteEntity(childEntity1);
     });
 
-    nitpick.addStep("Verify parent not deleted but all children are", function () {
+    nitpick.addStep("Verify only DOMAIN childEntity1 and AVATAR childEntity2 are deleted", function () {
         var failures = [];
         var properties = Entities.getEntityProperties(parentEntity);
         var parentEntityExists = (0 != Object.keys(properties).length);
@@ -88,20 +88,20 @@ nitpick.perform("Delete protocol sanity - TEST REQUIRES SERVER", Script.resolveP
         ++line;
         properties = Entities.getEntityProperties(childEntity1);
         var childEntity1Exists = (0 != Object.keys(properties).length);
-        console.warn(line, "childEntity1 deleted", !childEntity1Exists ? "pass" : "fail");
+        console.warn(line, "DOMAIN childEntity1 deleted", !childEntity1Exists ? "pass" : "fail");
         failures.push(childEntity1Exists);
 
         ++line;
         properties = Entities.getEntityProperties(childEntity2);
         var childEntity2Exists = (0 != Object.keys(properties).length);
-        console.warn(line, "childEntity2 deleted", !childEntity2Exists ? "pass" : "fail");
+        console.warn(line, "AVATAR childEntity2 deleted", !childEntity2Exists ? "pass" : "fail");
         failures.push(childEntity2Exists);
 
         ++line;
         properties = Entities.getEntityProperties(childEntity3);
         var childEntity3Exists = (0 != Object.keys(properties).length);
-        console.warn(line, "childEntity3 deleted", !childEntity3Exists ? "pass" : "fail");
-        failures.push(childEntity3Exists);
+        console.warn(line, "DOMAIN childEntity3 not deleted", childEntity3Exists ? "pass" : "fail");
+        failures.push(!childEntity3Exists);
 
         saveResults(failures);
     });
